3062f49f73579b62d5199a9dbe8605f21a088f49
CGA-PMMG <168194556+CGA-PMMG@users.noreply.github.com>
2024-04-29 16:26:30 -0300

    Update HISTORICO_OCORRENCIAS.sql

--- HISTORICO_OCORRENCIAS.sql
+++ HISTORICO_OCORRENCIAS.sql
@@ -2,29 +2,26 @@
  * Este código SQL é elaborado para fornecer informações de ocorrências policiais registradas 
  * pela Polícia Militar de Minas Gerais, filtrando palavras-chave no campo Histórico Ocorrência.
  -----------------------------------------------------------------------------------------------------------------------*/
--- SELEÇÃO DE CAMPOS RELEVANTES PARA ANÁLISE DAS OCORRÊNCIAS
 SELECT 
-    numero_ocorrencia,  -- NÚMERO IDENTIFICADOR DA OCORRÊNCIA
-    nome_tipo_relatorio,  -- TIPO DE RELATÓRIO GERADO
-    natureza_descricao_longa,  -- DESCRIÇÃO DETALHADA DA NATUREZA DA OCORRÊNCIA
-    data_hora_fato,  -- DATA E HORA EM QUE O FATO OCORREU
-    numero_latitude,  -- LATITUDE DO LOCAL DA OCORRÊNCIA
-    unidade_responsavel_registro_nome,  -- UNIDADE RESPONSÁVEL PELO REGISTRO DA OCORRÊNCIA
-    nome_municipio,  -- MUNICÍPIO ONDE OCORREU O FATO
-    historico_ocorrencia  -- HISTÓRICO DETALHADO DA OCORRÊNCIA
--- TABELA DE ONDE OS DADOS SÃO EXTRAÍDOS
-FROM db_bisp_reds_reporting.tb_ocorrencia
--- CONDIÇÕES PARA SELEÇÃO DE DADOS
+    OCO.numero_ocorrencia,  -- NÚMERO IDENTIFICADOR DA OCORRÊNCIA
+    OCO.nome_tipo_relatorio,  -- TIPO DE RELATÓRIO GERADO
+    OCO.natureza_descricao_longa,  -- DESCRIÇÃO DETALHADA DA NATUREZA DA OCORRÊNCIA
+    OCO.data_hora_fato,  -- DATA E HORA EM QUE O FATO OCORREU
+    OCO.numero_latitude,  -- LATITUDE DO LOCAL DA OCORRÊNCIA
+    OCO.unidade_responsavel_registro_nome,  -- UNIDADE RESPONSÁVEL PELO REGISTRO DA OCORRÊNCIA
+    OCO.nome_municipio,  -- MUNICÍPIO ONDE OCORREU O FATO
+    OCO.historico_ocorrencia  -- HISTÓRICO DETALHADO DA OCORRÊNCIA
+FROM db_bisp_reds_reporting.tb_ocorrencia OCO
 WHERE 
-    YEAR(data_hora_fato) BETWEEN 2018 AND 2022  -- INTERVALO TEMPORAL DAS OCORRÊNCIAS
-    AND relator_sigla_orgao = 'PM'  -- OCORRÊNCIAS REGISTRADAS PELA POLÍCIA MILITAR
-    AND ocorrencia_uf = 'MG'  -- OCORRÊNCIAS NO ESTADO DE MINAS GERAIS
-    AND descricao_estado = 'FECHADO'  -- SOMENTE OCORRÊNCIAS QUE JÁ FORAM FINALIZADAS (FECHADAS)
-    AND natureza_codigo = 'X99000'  -- CÓDIGO ESPECÍFICO PARA NATUREZA
+    YEAR(OCO.data_hora_fato) BETWEEN 2018 AND 2022  -- INTERVALO TEMPORAL DAS OCORRÊNCIAS
+    AND OCO.relator_sigla_orgao = 'PM'  -- OCORRÊNCIAS REGISTRADAS PELA POLÍCIA MILITAR
+    AND OCO.ocorrencia_uf = 'MG'  -- OCORRÊNCIAS NO ESTADO DE MINAS GERAIS
+    AND OCO.descricao_estado = 'FECHADO'  -- SOMENTE OCORRÊNCIAS QUE JÁ FORAM FINALIZADAS (FECHADAS)
+    AND OCO.natureza_codigo = 'X99000'  -- CÓDIGO ESPECÍFICO PARA NATUREZA
     AND (
-        historico_ocorrencia LIKE '%xxxxxxxxxx%'  -- FILTRA TEXTOS QUE MENCIONAM X
-        OR historico_ocorrencia LIKE '%xxxxxxxxxx%'  -- FILTRA TEXTOS QUE MENCIONAM X
-         )
-   -- AND unidade_area_militar_nome LIKE '%x BPM/x RPM%' -- FILTRE SUA BPM/ RPM 
-   -- AND nome_municipio  LIKE '%BELO HOR%'-- FILTRE O MUNICIPIO
+	       OCO.historico_ocorrencia LIKE '%xxxxxxxxxx%'  -- FILTRA TEXTOS QUE MENCIONAM X
+	       OR OCO.historico_ocorrencia LIKE '%xxxxxxxxxx%'  -- FILTRA TEXTOS QUE MENCIONAM X
+        )
+   -- AND OCO.unidade_area_militar_nome LIKE '%x BPM/x RPM%' -- FILTRE SUA BPM/ RPM 
+   -- AND OCO.nome_municipio  LIKE '%BELO HOR%'-- FILTRE O MUNICIPIO
     ;
