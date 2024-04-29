3ed25db3d5f4869cc4977f7ef481d4de627770e6
CGA-PMMG <168194556+CGA-PMMG@users.noreply.github.com>
2024-04-29 16:22:42 -0300

    Update ENVOLVIDOS_PRODUTIVIDADE.sql

--- ENVOLVIDOS_PRODUTIVIDADE.sql
+++ ENVOLVIDOS_PRODUTIVIDADE.sql
@@ -1,31 +1,30 @@
 /*---------------------------------------------------------------------------------------------------------------------
  * O objetivo deste código SQL é extrair informações detalhadas sobre indivíduos envolvidos em ocorrências policiais
  * registradas entre os anos de 2018 e 2022. A consulta foca em ocorrências onde as unidades responsáveis pelo 
- * registro são especificamente relacionadas à Polícia Rodoviária, como indicado pelas siglas 
- * 'CPRV' (Companhia de Polícia Rodoviária) e 'BPMRV' (Batalhão de Polícia Militar Rodoviária).
+ * registro são especificamente relacionadas XXXX
  ---------------------------------------------------------------------------------------------------------------------*/
 -- Seleciona colunas relacionadas aos envolvidos nas ocorrências registradas
 SELECT 
-    db_bisp_reds_reporting.tb_envolvido_ocorrencia.numero_envolvido,  -- Número identificador do envolvido na ocorrência
-    db_bisp_reds_reporting.tb_envolvido_ocorrencia.numero_ocorrencia,  -- Número identificador da ocorrência
-    db_bisp_reds_reporting.tb_envolvido_ocorrencia.natureza_ocorrencia_codigo,  -- Código da natureza da ocorrência associada ao envolvido
-    db_bisp_reds_reporting.tb_envolvido_ocorrencia.ind_consumado,  -- Indicador se a ocorrência foi consumada ou tentada
-    db_bisp_reds_reporting.tb_envolvido_ocorrencia.condicao_fisica_descricao,  -- Descrição da condição física do envolvido
-    db_bisp_reds_reporting.tb_envolvido_ocorrencia.envolvimento_descricao,  -- Descrição do tipo de envolvimento na ocorrência (ex: suspeito, vítima)
-    db_bisp_reds_reporting.tb_envolvido_ocorrencia.ind_mandado_prisao,  -- Indica se havia um mandado de prisão para o envolvido
-    db_bisp_reds_reporting.tb_envolvido_ocorrencia.ind_origem_sip,  -- Indica se a informação veio do sistema SIP (Sistema de Informações Policiais)
-    db_bisp_reds_reporting.tb_envolvido_ocorrencia.tipo_prisao_apreensao_descricao,  -- Descrição do tipo de prisão ou apreensão
-    db_bisp_reds_reporting.tb_envolvido_ocorrencia.ind_mandado_prisao_atual,  -- Indica se atualmente há um mandado de prisão
-    db_bisp_reds_reporting.tb_envolvido_ocorrencia.id_indicador_uso_etilometro,  -- Indicador de uso de etilômetro (bafômetro)
-    db_bisp_reds_reporting.tb_envolvido_ocorrencia.data_hora_fato  -- Data e hora do fato ocorrido
+    ENV.numero_envolvido,  -- Número identificador do envolvido na ocorrência
+    ENV.numero_ocorrencia,  -- Número identificador da ocorrência
+    ENV.natureza_ocorrencia_codigo,  -- Código da natureza da ocorrência associada ao envolvido
+    ENV.ind_consumado,  -- Indicador se a ocorrência foi consumada ou tentada
+    ENV.condicao_fisica_descricao,  -- Descrição da condição física do envolvido
+    ENV.envolvimento_descricao,  -- Descrição do tipo de envolvimento na ocorrência (ex: suspeito, vítima)
+    ENV.ind_mandado_prisao,  -- Indica se havia um mandado de prisão para o envolvido
+    ENV.ind_origem_sip,  -- Indica se a informação veio do sistema SIP (Sistema de Informações Policiais)
+    ENV.tipo_prisao_apreensao_descricao,  -- Descrição do tipo de prisão ou apreensão
+    ENV.ind_mandado_prisao_atual,  -- Indica se atualmente há um mandado de prisão
+    ENV.id_indicador_uso_etilometro,  -- Indicador de uso de etilômetro (bafômetro)
+    ENV.data_hora_fato  -- Data e hora do fato ocorrido
 FROM 
-    db_bisp_reds_reporting.tb_envolvido_ocorrencia  -- Tabela que contém os dados dos envolvidos nas ocorrências
+    db_bisp_reds_reporting.tb_envolvido_ocorrencia  ENV 
 LEFT JOIN 
-    db_bisp_reds_reporting.tb_ocorrencia  -- Tabela que contém os dados das ocorrências
-    ON db_bisp_reds_reporting.tb_envolvido_ocorrencia.numero_ocorrencia = db_bisp_reds_reporting.tb_ocorrencia.numero_ocorrencia  -- Condição para unir as tabelas pelo número da ocorrência
+    db_bisp_reds_reporting.tb_ocorrencia OCO
+    ON ENV.numero_ocorrencia = OCO.numero_ocorrencia  
 WHERE 
-    YEAR(db_bisp_reds_reporting.tb_ocorrencia.data_hora_fato) BETWEEN 2018 AND 2022  -- Filtra ocorrências entre os anos de 2018 e 2022
+    YEAR(OCO.data_hora_fato) BETWEEN 2018 AND 2022  -- Filtra ocorrências entre os anos de 2018 e 2022
     AND (
-        db_bisp_reds_reporting.tb_ocorrencia.unidade_responsavel_registro_nome LIKE '%XXXX%' OR  -- Filtra ocorrências onde a unidade responsável contém 'XXXX'
-        db_bisp_reds_reporting.tb_ocorrencia.unidade_responsavel_registro_nome LIKE '%XXXX%'  -- Ou filtra ocorrências onde a unidade responsável contém 'XXXX'
-    );
+        OCO.unidade_responsavel_registro_nome LIKE '%XXXX%' OR  -- 
+        OCO.unidade_responsavel_registro_nome LIKE '%XXXX%'  -- Filtra ocorrências onde a unidade responsável contém 'XXXX' Ouunidade responsável contém 'XXXX'
+    	);
