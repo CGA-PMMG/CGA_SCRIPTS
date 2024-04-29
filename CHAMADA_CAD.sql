619ba13aeef9fd77eee07988fc45b56cd25c2154
CGA-PMMG <168194556+CGA-PMMG@users.noreply.github.com>
2024-04-29 15:16:35 -0300

    Update CHAMADA_CAD.sql

--- CHAMADA_CAD.sql
+++ CHAMADA_CAD.sql
@@ -3,50 +3,35 @@
  ---------------------------------------------------------------------------------------------------------*/
 -- Seleciona o ID e o nome do município das chamadas de atendimento
 SELECT 
-    CA.local_municipio_id, 
-    CA.local_municipio_nome,
-    -- Conta o número distinto de chamadas para o ano de 2023
-    COUNT(DISTINCT CASE WHEN YEAR(CA.chamada_data_hora_inclusao) = 2023 THEN CA.chamada_id END) AS '2023',   
-    -- Conta o número distinto de chamadas para o ano de 2024
-    COUNT(DISTINCT CASE WHEN YEAR(CA.chamada_data_hora_inclusao) = 2024 THEN CA.chamada_id END) AS '2024'  
--- Indica a tabela de onde os dados serão retirados
+    CA.local_municipio_id, -- ID do município
+    CA.local_municipio_nome, -- Nome do município
+    COUNT(DISTINCT CASE WHEN YEAR(CA.chamada_data_hora_inclusao) = 2023 THEN CA.chamada_id END) AS '2023',  -- Conta o número distinto de chamadas para o ano de 2023
+    COUNT(DISTINCT CASE WHEN YEAR(CA.chamada_data_hora_inclusao) = 2024 THEN CA.chamada_id END) AS '2024'   -- Conta o número distinto de chamadas para o ano de 2024
 FROM tb_chamada_atendimento CA
--- Define as condições para a seleção dos dados
 WHERE 
-    -- Seleciona chamadas entre o início de 2023 e o final de março de 2024
-    (CA.chamada_data_hora_inclusao) BETWEEN '2023-01-01' AND '2024-03-31'
-    -- Filtra as chamadas atendidas pela Polícia Militar
-    AND CA.orgao_sigla = 'PM'
--- Agrupa os resultados pelo ID e nome do município
-GROUP BY CA.local_municipio_id, CA.local_municipio_nome
--- Ordena os resultados primeiro pelo nome do município e depois pelo ID do município
-ORDER BY CA.local_municipio_nome, CA.local_municipio_id;
+    CA.chamada_data_hora_inclusao BETWEEN '2023-01-01' AND '2024-03-31'  -- Seleciona chamadas entre o início de 2023 e o final de março de 202
+    AND CA.orgao_sigla = 'PM'  -- Filtra as chamadas atendidas pela Polícia Militar
+GROUP BY CA.local_municipio_id, CA.local_municipio_nome -- Agrupa os resultados pelo ID e nome do município
+ORDER BY CA.local_municipio_nome, CA.local_municipio_id; -- Ordena os resultados primeiro pelo nome do município e depois pelo ID do município
 
 
 
 /*----------------------------------------------------------------------------------------------------------
- * Contagem de chamadas realizadas no CAD em 2023 e 2024 atendidas pela unidade específica
+ * Contagem de chamadas realizadas no CAD em 2023 e 2024 atendidas pelo recurso específico
  ---------------------------------------------------------------------------------------------------------*/
 SELECT 
-    CA.local_municipio_id, 
-    CA.local_municipio_nome,
-    COUNT(DISTINCT CASE WHEN YEAR(CA.chamada_data_hora_inclusao) = 2023 THEN CA.chamada_id END) AS '2023',   
-    COUNT(DISTINCT CASE WHEN YEAR(CA.chamada_data_hora_inclusao) = 2024 THEN CA.chamada_id END) AS '2024'    
--- Especifica a tabela principal de onde os dados serão retirados
+    CA.local_municipio_id, -- ID do município
+    CA.local_municipio_nome, -- Nome do município
+    COUNT(DISTINCT CASE WHEN YEAR(CA.chamada_data_hora_inclusao) = 2023 THEN CA.chamada_id END) AS '2023',   -- Conta o número distinto de chamadas para o ano de 2023
+    COUNT(DISTINCT CASE WHEN YEAR(CA.chamada_data_hora_inclusao) = 2024 THEN CA.chamada_id END) AS '2024'    -- Conta o número distinto de chamadas para o ano de 2024
 FROM tb_chamada_atendimento CA
--- Realiza uma junção INNER JOIN com a tabela de empenhos para filtrar as chamadas que têm empenhos associados
 INNER JOIN tb_empenho EMP 
 ON CA.chamada_id = EMP.chamada_atendimento_id
--- Define as condições para a seleção dos dados
 WHERE 
-    -- Seleciona chamadas entre o início de 2023 e o final de março de 2024
-    (CA.chamada_data_hora_inclusao) BETWEEN '2023-01-01' AND '2024-03-31'
-    -- Filtra as chamadas atendidas pela Polícia Militar
-    AND CA.orgao_sigla = 'PM'
-    -- Filtra por empenhos que usam o tipo de recurso com código 'XX'
-    AND EMP.tipo_recurso_codigo = 'XX'
--- Agrupa os resultados pelo ID e nome do município para contagem
-GROUP BY CA.local_municipio_id, CA.local_municipio_nome
--- Ordena os resultados pelo nome do município e pelo ID do município para facilitar a visualização
-ORDER BY CA.local_municipio_nome, CA.local_municipio_id;
+    (CA.chamada_data_hora_inclusao) BETWEEN '2023-01-01' AND '2024-03-31'     -- Seleciona chamadas entre o início de 2023 e o final de março de 2024
+    AND CA.orgao_sigla = 'PM'    -- Filtra as chamadas atendidas pela Polícia Militar
+    AND EMP.tipo_recurso_codigo = 'XX'   -- Filtra por empenhos que usam o tipo de recurso com código 'XX'
+GROUP BY CA.local_municipio_id, CA.local_municipio_nome -- Agrupa os resultados pelo ID e nome do município para contagem
+ORDER BY CA.local_municipio_nome, CA.local_municipio_id; -- Ordena os resultados pelo nome do município e pelo ID do município para facilitar a visualização
+
 
