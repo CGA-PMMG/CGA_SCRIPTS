f5dcaca71b29bf80b49b11645a143e86f075a9af
CGA-PMMG <168194556+CGA-PMMG@users.noreply.github.com>
2024-07-16 16:39:06 -0300

    Update REDS_RAT_EFETIVOS.sql

--- REDS_RAT_EFETIVOS.sql
+++ REDS_RAT_EFETIVOS.sql
@@ -1,5 +1,6 @@
-﻿/*-------------------------------------------------------------------------------------------------------------------
-O objetivo deste código SQL é extrair informações sobre os efetivos empenhado no RAT. Incluindo o número da ocorrência, matrícula, nome, cargo do digitador, e  unidade responsável.
+/*-----------------------------------------------------------------------------------------------------------------
+ * O objetivo deste código SQL é extrair informações sobre os efetivos empenhado no RAT. Incluindo o 
+ * número da ocorrência, matrícula, nome, cargo do digitador, e  unidade responsável.
 --------------------------------------------------------------------------------------------------------------------*/
             SELECT	
                 OCO.numero_ocorrencia AS 'NUM_ATIVIDADE',  -- SELECIONA O NÚMERO DA OCORRÊNCIA 
@@ -7,8 +8,10 @@ O objetivo deste código SQL é extrair informações sobre os efetivos empenhad
                 OCO.digitador_nome AS 'NOME',  -- SELECIONA O NOME DO DIGITADOR
                 OCO.digitador_cargo_efetivo AS 'CARGO',  -- SELECIONA O CARGO EFETIVO DO DIGITADOR 
                 OCO.unidade_responsavel_registro_codigo AS 'COD_UNIDADE_SERVICO',  -- SELECIONA O CÓDIGO DA UNIDADE RESPONSÁVEL PELO REGISTRO
-                OCO.unidade_responsavel_registro_nome AS 'NOME_UNIDADE'  -- SELECIONA O NOME DA UNIDADE RESPONSÁVEL PELO REGISTRO
-            FROM
+                OCO.unidade_responsavel_registro_nome AS 'NOME_UNIDADE',  -- SELECIONA O NOME DA UNIDADE RESPONSÁVEL PELO REGISTRO
+            OCO.data_hora_alteracao,
+            OCO.data_hora_fato
+                FROM
                 db_bisp_reds_reporting.tb_ocorrencia OCO  
             WHERE 1=1  -- CONDIÇÃO SEMPRE VERDADEIRA PARA FACILITAR A ADIÇÃO DE OUTRAS CONDIÇÕES
          	AND OCO.digitador_sigla_orgao = 'PM'  
@@ -17,6 +20,5 @@ O objetivo deste código SQL é extrair informações sobre os efetivos empenhad
             AND OCO.data_hora_fato IS NOT NULL -- FILTRA OCORRÊNCIAS ONDE A DATA/HORA DO FATO NÃO SÃO NULAS
             AND OCO.data_hora_alteracao  BETWEEN '2024-01-01' AND '2024-05-01'  -- FILTRA OCORRÊNCIAS ONDE A DATA/HORA DA ALTERAÇÃO ESTÃO NO INTERVALO ESPECIFICADO
 --          AND OCO.unidade_responsavel_registro_nome LIKE '%/X RPM' -- FILTRE A UEOP - GP/PL/CIA/BPM/RPM
---	    AND OCO.digitador_matricula ='XXXX' -- FILTRE A MATRÍCULA DO DIGITADOR
-            ORDER BY OCO.data_hora_fato;  -- ORDENA OS RESULTADOS PELA DATA/HORA DO FATO
-
+--	        AND OCO.digitador_matricula ='XXXX' -- FILTRE A MATRÍCULA DO DIGITADOR
+            ORDER BY OCO.data_hora_fato  -- ORDENA OS RESULTADOS PELA DATA/HORA DO FATO;
