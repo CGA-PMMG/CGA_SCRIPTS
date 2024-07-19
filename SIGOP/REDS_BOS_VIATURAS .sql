4a955d21303d4188f65f369af916c100f95ce2a2
CGA-PMMG <168194556+CGA-PMMG@users.noreply.github.com>
2024-07-19 10:49:25 -0300

    Update REDS_BOS_VIATURAS .sql

--- SIGOP/REDS_BOS_VIATURAS .sql
+++ SIGOP/REDS_BOS_VIATURAS .sql
@@ -7,8 +7,7 @@
                 VTR.numero_sequencial_viatura AS 'NUM_SEQ_RECURSO',  -- SELECIONA O NÚMERO SEQUENCIAL DA VIATURA
                 VTR.placa AS 'NUM_PLACA',  -- SELECIONA A PLACA DA VIATURA 
                 VTR.numero_reg AS 'NUM_PREFIXO' , -- SELECIONA O NÚMERO DE REGISTRO DA VIATURA 
-                OCO.unidade_responsavel_registro_nome 'UEOP'-- SELECIONA A UNIDADE RESPONSÁVEL PELO REGRISTRO
-            FROM
+		    FROM
                 db_bisp_reds_reporting.tb_ocorrencia AS OCO  
             LEFT JOIN db_bisp_reds_reporting.tb_viatura_ocorrencia VTR  
                 ON  OCO.numero_ocorrencia = VTR.numero_ocorrencia  
@@ -18,7 +17,6 @@
             AND OCO.ind_estado IN ('R', 'F')  -- FILTRA OCORRÊNCIAS ONDE O ESTADO É RECEBIDO OU FECHADO
 			AND OCO.data_hora_fato IS NOT NULL -- FILTRA OCORRÊNCIAS ONDE A DATA/HORA DO FATO NÃO SÃO NULAS
             AND VTR.data_hora_fato  BETWEEN '2024-01-01' AND '2024-02-01'  -- FILTRA OCORRÊNCIAS ONDE A DATA/HORA DO FATO ESTÃO NO INTERVALO ESPECIFICADO
---          AND OCO.unidade_responsavel_registro_nome LIKE '%/X RPM' -- FILTRE A UEOP - GP/PL/CIA/BPM/RPM
 -- 	        AND VTR.placa = 'XXX' -- FILTRE PELA PLACA DA VIATURA
 -- 	        AND OCO.numero_ocorrencia = 'XXX'--FILTRE PELO NÚMERO DO BOS/BOS AMPLO
             ORDER BY VTR.data_hora_fato  -- ORDENA OS RESULTADOS PELA DATA E HORA DO FATO
