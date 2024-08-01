89d316ecbb9432d079c2cec56800a5599ea6353d
CGA-PMMG <168194556+CGA-PMMG@users.noreply.github.com>
2024-08-01 11:33:36 -0300

    Update FURTO_ROUBO_VEICULO.sql

--- FURTO_ROUBO_VEICULO.sql
+++ FURTO_ROUBO_VEICULO.sql
@@ -2,7 +2,6 @@
 -- OBJETIVO: EXTRAIR DADOS DETALHADOS DE OCORRÊNCIAS ESPECÍFICAS ENVOLVENDO VEÍCULOS E
 -- ENVOLVIDOS
 -----------------------------------------------------------------------------------------------*/
--- SELEÇÃO DE INFORMAÇÕES ESPECÍFICAS SOBRE ENVOLVIDOS, OCORRÊNCIAS E VEÍCULOS
 SELECT DISTINCT
     ENV.nome_completo_envolvido, -- NOME COMPLETO DO ENVOLVIDO
     OCO.numero_ocorrencia, -- NÚMERO DA OCORRÊNCIA
@@ -10,10 +9,10 @@ SELECT DISTINCT
     VEI.tipo_veiculo_descricao, -- DESCRIÇÃO DO TIPO DE VEÍCULO ENVOLVIDO
     ENV.tipo_prisao_apreensao_descricao -- DESCRIÇÃO DO TIPO DE PRISÃO OU APREENSÃO
 -- TABELAS UTILIZADAS PARA A CONSULTA COM AS DEVIDAS JUNÇÕES
-FROM tb_ocorrencia OCO
-INNER JOIN tb_envolvido_ocorrencia ENV  
+FROM db_bisp_reds_reporting.tb_ocorrencia OCO
+INNER JOIN db_bisp_reds_reporting.tb_envolvido_ocorrencia ENV  
     ON ENV.numero_ocorrencia = OCO.numero_ocorrencia -- JUNÇÃO PARA ASSOCIAR ENVOLVIDOS ÀS OCORRÊNCIAS
-INNER JOIN tb_veiculo_ocorrencia VEI 
+INNER JOIN db_bisp_reds_reporting.tb_veiculo_ocorrencia VEI 
     ON VEI.numero_ocorrencia = OCO.numero_ocorrencia -- JUNÇÃO PARA ASSOCIAR VEÍCULOS ÀS OCORRÊNCIAS
 -- CONDIÇÕES ESPECÍFICAS PARA FILTRAR OS DADOS DE OCORRÊNCIAS
 WHERE YEAR(OCO.data_hora_fato) BETWEEN 2023 AND 2024 -- OCORRÊNCIAS ENTRE OS ANOS DE 2023 E 2024
@@ -26,3 +25,4 @@ WHERE YEAR(OCO.data_hora_fato) BETWEEN 2023 AND 2024 -- OCORRÊNCIAS ENTRE OS AN
    -- AND OCO.nome_municipio  LIKE '%BELO HOR%'-- FILTRE O MUNICIPIO
 -- ORDENAMENTO DOS RESULTADOS
 ORDER BY 1,2,4,5; -- ORDEM ALFABÉTICA E NUMÉRICA BASEADA EM NOME, NÚMERO DE OCORRÊNCIA, TIPO DE VEÍCULO E TIPO DE PRISÃO/APREENSÃO
+
