efaa595015c0f73c45eb98cbe4b190067fd75800
CGA-PMMG <168194556+CGA-PMMG@users.noreply.github.com>
2024-09-24 11:01:28 -0300

    Update MILITAR_RAT_PRODUTIVIDADE.sql

--- PRODUTIVIDADE/MILITAR_RAT_PRODUTIVIDADE.sql
+++ PRODUTIVIDADE/MILITAR_RAT_PRODUTIVIDADE.sql
@@ -1,4 +1,9 @@
-
+/*------------------------------------------------------------------------------------------------------------------
+ * Este script SQL tem como finalidade fornecer uma visão detalhada dos membros das equipes que atuaram em 
+ * ocorrências relacionadas ao RAT Produtividade, destacando suas respectivas funções, unidades de lotação, 
+ * a descrição do indicador do Rat e sua quantidade. A consulta integra informações das tabelas de 
+ * integrante da guarnição, ocorrências e RAT produtividade, permitindo uma análise focada no desempenho de equipes. 
+--------------------------------------------------------------------------------------------------------------------*/
 SELECT 
     db_bisp_reds_reporting.tb_integrante_guarnicao_ocorrencia.numero_matricula, -- Matrícula do integrante
     db_bisp_reds_reporting.tb_integrante_guarnicao_ocorrencia.nome_cargo, -- Cargo do integrante
@@ -22,7 +27,7 @@ WHERE
     AND MONTH (db_bisp_reds_reporting.tb_ocorrencia.data_hora_fato) = 1 -- Filtra o mês da data/hora fato
     AND db_bisp_reds_reporting.tb_ocorrencia.ocorrencia_uf = 'MG' -- Filtra ocorrências no estado de MG
     AND db_bisp_reds_reporting.tb_ocorrencia.unidade_responsavel_registro_nome LIKE '%X BPM%'  -- Filtra ocorrências da unidade responsável pelo registro
-    AND db_bisp_reds_reporting.tb_ocorrencia.nome_municipio  LIKE '%XXX%'-- Filtra município da ocorrência
+    AND UPPER(db_bisp_reds_reporting.tb_ocorrencia.nome_municipio)  LIKE UPPER('%XXXX%')-- Filtra município da ocorrência, em letras maiúsculas
     AND db_bisp_reds_reporting.tb_ocorrencia.digitador_id_orgao = 0 -- Garante que retorne ocorrências atendidas apenas por policiais militares
    -- AND db_bisp_reds_reporting.tb_integrante_guarnicao_ocorrencia.numero_matricula = 'xxxx' -- Filtre pela matrícula do militar 
 ORDER BY numero_matricula, data_hora_fato, numero_ocorrencia -- Ordena por número da matricula do militar, data/hora do fato e pelo número da ocorrência
