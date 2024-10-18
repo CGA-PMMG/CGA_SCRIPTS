1cd80a17428cb04c7a18fd7e741605ade47ab789
CGA-PMMG <168194556+CGA-PMMG@users.noreply.github.com>
2024-10-18 10:25:48 -0300

    Add files via upload

--- PPVD/VITIMA_VD_REINCIDENTE.sql
+++ PPVD/VITIMA_VD_REINCIDENTE.sql
@@ -41,6 +41,8 @@ WITH REINCIDENCIA_VD AS
         AND OCO.natureza_codigo NOT LIKE 'A20%' -- Exclui ocorrências cuja natureza comece com 'A20'
         AND OCO.relator_sigla_orgao = 'PM' -- Filtra ocorrências registradas pela Polícia Militar
         AND OCO.ocorrencia_uf = 'MG' -- Filtra ocorrências que ocorreram no estado de Minas Gerais
+        --AND OCO.codigo_municipio IN (0,0,0,...) -- Filtre pelo código de município desejado
+        --AND unidade_responsavel_registro_nome LIKE '%BELO HORI%' --Filtre pelo nome de município desejado
 )
 SELECT  
     UPPER(RVD.nome_completo_envolvido) as nome_completo_envolvido, -- Seleciona e converte o nome completo do envolvido para letras maiúsculas
