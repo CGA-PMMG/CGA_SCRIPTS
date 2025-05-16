b737bd615441ba1a1ee15102343dc451cef768a5
CGA-PMMG <168194556+CGA-PMMG@users.noreply.github.com>
2025-05-16 16:33:19 -0300

    Update VTCV_DENOMINADOR.sql

--- INDICADORES DE INTERAÇÃO COMUNITÁRIA - DOP/VTCV_DENOMINADOR.sql
+++ INDICADORES DE INTERAÇÃO COMUNITÁRIA - DOP/VTCV_DENOMINADOR.sql
@@ -164,10 +164,10 @@ AND EXISTS (
    AND envolvido.id_envolvimento IN(25,1097, 27, 32, 28, 26, 872)        -- Filtro por códigos específicos de envolvimento -  vítimas
    AND envolvido.condicao_fisica_codigo <> '0100'                         -- Filtro por condição física difernte de fatal (código '0100')
 )                                                                          -- Fim da subconsulta EXISTS - exclui vítima fatal
-AND OCO.data_hora_fato BETWEEN '2024-01-01 00:00:00.000' AND '2025-02-28 23:59:59.000' -- Filtra ocorrências por período específico (todo o ano de 2024 até fevereiro/2025)
+AND OCO.data_hora_fato BETWEEN '2024-01-01 00:00:00.000' AND '2025-04-30 23:59:59.000' -- Filtra ocorrências por período específico (todo o ano de 2024 até fevereiro/2025)
 AND OCO.ocorrencia_uf = 'MG'                                                     -- Filtra apenas ocorrências do estado de Minas Gerais
 AND OCO.digitador_sigla_orgao IN ('PM','PC')                              -- Filtro por ocorrências, Polícia Militar ou Polícia Civil
-AND OCO.natureza_codigo IN('B01121','B01148','B02001','C01157','C01158','B01504') -- Seleção de naturezas do CV
+AND OCO.natureza_codigo IN('B01121','B01148','B02001','C01157','C01158','C01159','B01504') -- Seleção de naturezas do CV
 AND OCO.ind_estado = 'F'                                                         -- Filtra apenas ocorrências fechadas
 --AND OCO.unidade_area_militar_nome LIKE '%X BPM/X RPM%'   -- FILTRE PELO NOME DA UNIDADE AREA MILITAR
 ORDER BY  OCO.numero_ocorrencia
