0a2467c8b1a16b540fad8e2024f08cd8e6eca554
CGA-PMMG <168194556+CGA-PMMG@users.noreply.github.com>
2025-05-28 11:14:03 -0300

    Update GDO_2025_CVPe.sql

--- GDO 2025/GDO_2025_CVPe.sql
+++ GDO 2025/GDO_2025_CVPe.sql
@@ -209,6 +209,8 @@ WHERE 1=1                                                           -- Início d
     AND YEAR(OCO.data_hora_fato) BETWEEN 2020 AND 2024                   -- Filtra até o mês final
     AND OCO.ind_estado = 'F'                                      -- Filtra apenas ocorrências finalizadas
     AND OCO.local_imediato_codigo NOT IN('1302','1310')		-- Filtra ocorrências cujo local imediato nâo seja UNIDADE PRISIONAL (CERESP/PRESIDIO/PENITENCIARIA) ou CAEDEIA PUBLICA
+-- AND OCO.codigo_municipio IN (123456,456789,987654,......) -- PARA RESGATAR APENAS OS DADOS DOS MUNICÍPIOS SOB SUA RESPONSABILIDADE, REMOVA O COMENTÁRIO E ADICIONE O CÓDIGO DE MUNICIPIO DA SUA RESPONSABILIDADE. NO INÍCIO DO SCRIPT, É POSSÍVEL VERIFICAR ESSES CÓDIGOS, POR RPM E UEOP.
+   -- AND OCO.unidade_area_militar_nome LIKE '%x BPM/x RPM%' -- Filtra pelo nome da unidade área militar
   ORDER BY                                                           -- Define a ordem de apresentação dos resultados
     --RPM_2025,                                                     -- Primeiro por RPM
     --UEOP_2025,                                                    -- Depois por UEOP
