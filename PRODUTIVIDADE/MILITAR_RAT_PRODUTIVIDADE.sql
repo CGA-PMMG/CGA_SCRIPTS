20ecd97ad0e93a5407290751c15edd71f0987cb3
CGA-PMMG <168194556+CGA-PMMG@users.noreply.github.com>
2025-06-05 11:03:41 -0300

    Update MILITAR_RAT_PRODUTIVIDADE.sql

--- PRODUTIVIDADE/MILITAR_RAT_PRODUTIVIDADE.sql
+++ PRODUTIVIDADE/MILITAR_RAT_PRODUTIVIDADE.sql
@@ -5,29 +5,29 @@
  * integrante da guarnição, ocorrências e RAT produtividade, permitindo uma análise focada no desempenho de equipes. 
 --------------------------------------------------------------------------------------------------------------------*/
 SELECT 
-    db_bisp_reds_reporting.tb_integrante_guarnicao_ocorrencia.numero_matricula, -- Matrícula do integrante
-    db_bisp_reds_reporting.tb_integrante_guarnicao_ocorrencia.nome_cargo, -- Cargo do integrante
-    db_bisp_reds_reporting.tb_integrante_guarnicao_ocorrencia.nome, -- Nome do integrante
-    db_bisp_reds_reporting.tb_integrante_guarnicao_ocorrencia.unidade_servico_nome, -- Nome da unidade de serviço
-    db_bisp_reds_reporting.tb_ocorrencia.unidade_responsavel_registro_nome, -- Nome da unidade responsável pelo registro
-    db_bisp_reds_reporting.tb_integrante_guarnicao_ocorrencia.data_hora_fato, -- Data/hora do fato
-    db_bisp_reds_reporting.tb_integrante_guarnicao_ocorrencia.numero_ocorrencia, -- Número da ocorrência
-    db_bisp_reds_reporting.tb_rat_produtividade_ocorrencia.indicador_descricao, -- Descrição o indicador da produtividade
-    db_bisp_reds_reporting.tb_rat_produtividade_ocorrencia.quantidade  -- Quantidade produtividade
+    IG.numero_matricula, -- Matrícula do integrante
+    IG.nome_cargo, -- Cargo do integrante
+    IG.nome, -- Nome do integrante
+    IG.unidade_servico_nome, -- Nome da unidade de serviço
+    OCO.unidade_responsavel_registro_nome, -- Nome da unidade responsável pelo registro
+    IG.data_hora_fato, -- Data/hora do fato
+    IG.numero_ocorrencia, -- Número da ocorrência
+    RAT.indicador_descricao, -- Descrição o indicador da produtividade
+    RAT.quantidade  -- Quantidade produtividade
 FROM 
-    db_bisp_reds_reporting.tb_integrante_guarnicao_ocorrencia -- Tabela de integrantes de guarnição
-INNER JOIN  
-    db_bisp_reds_reporting.tb_ocorrencia -- Junção com a tabela de ocorrências para retornar dados da ocorrência 
-    ON db_bisp_reds_reporting.tb_ocorrencia.numero_ocorrencia = db_bisp_reds_reporting.tb_integrante_guarnicao_ocorrencia.numero_ocorrencia 
-INNER JOIN  
-    db_bisp_reds_reporting.tb_rat_produtividade_ocorrencia -- Junção com a tabela Rat Produtividade para retornar dados da produtividade do rat
-    ON db_bisp_reds_reporting.tb_rat_produtividade_ocorrencia.numero_ocorrencia = db_bisp_reds_reporting.tb_integrante_guarnicao_ocorrencia.numero_ocorrencia 
+    db_bisp_reds_reporting.tb_integrante_guarnicao_ocorrencia as IG-- Tabela de integrantes de guarnição
+ INNER JOIN  
+    db_bisp_reds_reporting.tb_ocorrencia as OCO-- Junção com a tabela de ocorrências para retornar dados da ocorrência 
+    ON OCO.numero_ocorrencia = IG.numero_ocorrencia 
+ INNER JOIN  
+    db_bisp_reds_reporting.tb_rat_produtividade_ocorrencia as RAT-- Junção com a tabela Rat Produtividade para retornar dados da produtividade do rat
+    ON RAT.numero_ocorrencia = IG.numero_ocorrencia 
 WHERE 
-    YEAR(db_bisp_reds_reporting.tb_ocorrencia.data_hora_fato) = 2018 -- Filtra ano da data/hora fato
-    AND MONTH (db_bisp_reds_reporting.tb_ocorrencia.data_hora_fato) = 1 -- Filtra o mês da data/hora fato
-    AND db_bisp_reds_reporting.tb_ocorrencia.ocorrencia_uf = 'MG' -- Filtra ocorrências no estado de MG
-    AND db_bisp_reds_reporting.tb_ocorrencia.unidade_responsavel_registro_nome LIKE '%X BPM%'  -- Filtra ocorrências da unidade responsável pelo registro
-    AND UPPER(db_bisp_reds_reporting.tb_ocorrencia.nome_municipio)  LIKE UPPER('%XXXX%')-- Filtra município da ocorrência, em letras maiúsculas
-    AND db_bisp_reds_reporting.tb_ocorrencia.digitador_id_orgao = 0 -- Garante que retorne ocorrências atendidas apenas por policiais militares
-   -- AND db_bisp_reds_reporting.tb_integrante_guarnicao_ocorrencia.numero_matricula = 'xxxx' -- Filtre pela matrícula do militar 
-ORDER BY numero_matricula, data_hora_fato, numero_ocorrencia -- Ordena por número da matricula do militar, data/hora do fato e pelo número da ocorrência
+    YEAR(OCO.data_hora_fato) = 2025 -- Filtra ano da data/hora fato
+    AND MONTH (OCO.data_hora_fato) = 6 -- Filtra o mês da data/hora fato
+    AND OCO.ocorrencia_uf = 'MG' -- Filtra ocorrências no estado de MG
+    AND OCO.digitador_id_orgao = 0 -- Garante que retorne ocorrências atendidas apenas por policiais militares
+	--  AND OCO.unidade_responsavel_registro_nome LIKE '%xx BPM/xx RPM%' -- Filtra pelo nome da unidade responsável pelo registro
+	--  AND OCO.codigo_municipio IN (123456,456789,987654,......) -- PARA RESGATAR APENAS OS DADOS DOS MUNICÍPIOS SOB SUA RESPONSABILIDADE, REMOVA O COMENTÁRIO E ADICIONE O CÓDIGO DE MUNICIPIO DA SUA RESPONSABILIDADE. NO INÍCIO DO SCRIPT, É POSSÍVEL VERIFICAR ESSES CÓDIGOS, POR RPM E UEOP.
+    --  AND IG.numero_matricula = 'xxxx' -- Filtre pela matrícula do militar 
+ORDER BY IG.numero_matricula, IG.data_hora_fato, IG.numero_ocorrencia -- Ordena por número da matricula do militar, data/hora do fato e pelo número da ocorrência
