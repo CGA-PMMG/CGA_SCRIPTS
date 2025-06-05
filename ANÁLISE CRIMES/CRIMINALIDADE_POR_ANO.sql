db70d65d414158010ecf228bd8bc432801142692
CGA-PMMG <168194556+CGA-PMMG@users.noreply.github.com>
2025-06-05 14:33:17 -0300

    Update CRIMINALIDADE_POR_ANO.sql

--- ANÁLISE CRIMES/CRIMINALIDADE_POR_ANO.sql
+++ ANÁLISE CRIMES/CRIMINALIDADE_POR_ANO.sql
@@ -17,15 +17,14 @@ SELECT
     COUNT(DISTINCT CASE WHEN YEAR(OCO.data_hora_fato) = 2024 THEN OCO.numero_ocorrencia END) AS '2024',  -- CONTADOR DE OCORRÊNCIAS EM 2022 
     COUNT(DISTINCT CASE WHEN YEAR(OCO.data_hora_fato) = 2025 THEN OCO.numero_ocorrencia END) AS '2025'  -- CONTADOR DE OCORRÊNCIAS EM 2022 
    FROM db_bisp_reds_reporting.tb_ocorrencia OCO  -- TABELA DE OCORRÊNCIAS COM ALIÁS "OCO"
-   LEFT JOIN db_bisp_reds_master.tb_local_unidade_area_pmmg LO ON OCO.id_local = LO.id_local
 WHERE 1=1 -- Filtro sempre verdadeiro, usado como ponto de partida para adicionar condições subsequentes
-AND OCO.data_hora_fato BETWEEN '2020-01-01 00:00:00.000' AND '2025-04-30 23:59:59.000' -- Filtra ocorrências por período específico (todo o ano de 2024 até fevereiro/2025)
-AND OCO.nome_tipo_relatorio IN ('POLICIAL', 'REFAP', 'TRANSITO')  -- FILTRA TIPOS ESPECÍFICOS DE RELATÓRIOS
-AND (SUBSTRING(OCO.natureza_codigo, 1, 1) IN ('B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'T')  -- FILTRA OCORRÊNCIAS POR CÓDIGOS DE NATUREZA ESPECÍFICOS
-	 AND OCO.natureza_codigo NOT IN ('T00007', 'T00008', 'T00009', 'T10161', 'T99000')  )-- EXCLUI CÓDIGOS DE NATUREZA ESPECÍFICOS'
 AND OCO.digitador_id_orgao IN (0, 1) -- Filtra registros digitados por órgãos (PM, PC)
 AND OCO.ind_estado = 'F' -- Filtra ocorrências onde o estado é fechado
 AND OCO.ocorrencia_uf = 'MG' -- Filtra ocorrências no estado de Minas Gerais
+AND OCO.nome_tipo_relatorio IN ('POLICIAL', 'REFAP', 'TRANSITO')  -- FILTRA TIPOS ESPECÍFICOS DE RELATÓRIOS
+AND (SUBSTRING(OCO.natureza_codigo, 1, 1) IN ('B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'T')  -- FILTRA OCORRÊNCIAS POR CÓDIGOS DE NATUREZA ESPECÍFICOS
+	 AND OCO.natureza_codigo NOT IN ('T00007', 'T00008', 'T00009', 'T10161', 'T99000')  )-- EXCLUI CÓDIGOS DE NATUREZA ESPECÍFICOS'
+AND OCO.data_hora_fato BETWEEN '2020-01-01 00:00:00.000' AND '2025-04-30 23:59:59.000' -- Filtra ocorrências por período específico (todo o ano de 2024 até fevereiro/2025)
   -- AND OCO.unidade_area_militar_nome LIKE '%x BPM/x RPM%' -- Filtra pelo nome da unidade área militar
 	-- AND OCO.unidade_responsavel_registro_nome LIKE '%xx RPM%' -- Filtra pelo nome da unidade responsável pelo registro
 	-- AND OCO.codigo_municipio IN (123456,456789,987654,......) -- PARA RESGATAR APENAS OS DADOS DOS MUNICÍPIOS SOB SUA RESPONSABILIDADE, REMOVA O COMENTÁRIO E ADICIONE O CÓDIGO DE MUNICIPIO DA SUA RESPONSABILIDADE. NO INÍCIO DO SCRIPT, É POSSÍVEL VERIFICAR ESSES CÓDIGOS, POR RPM E UEOP.
