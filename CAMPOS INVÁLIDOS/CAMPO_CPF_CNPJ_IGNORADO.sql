a987a849e29f22e0e5449a85f2bcf24cc04c3487
CGA-PMMG <168194556+CGA-PMMG@users.noreply.github.com>
2025-06-09 16:55:44 -0300

    Update CAMPO_CPF_CNPJ_IGNORADO.sql

--- CAMPOS INVÁLIDOS/CAMPO_CPF_CNPJ_IGNORADO.sql
+++ CAMPOS INVÁLIDOS/CAMPO_CPF_CNPJ_IGNORADO.sql
@@ -1,35 +1,35 @@
- /*---------------------------------------------------------------------------------------------------------------------
-  * Este código SQL tem como objetivo calcular e apresentar informações detalhadas sobre o total de envolvidos nas 
-  * ocorrências registradas pela Polícia Militar de Minas Gerais no ano de 2024 e, mais especificamente, quantificar 
-  * quantos desses registros estão sem CPF ou CNPJ informado. O foco é analisar ocorrências fechadas e filtrar por diversos 
-  * critérios para garantir a relevância e precisão dos dados, especialmente visando a clareza na identificação das unidades 
-  * responsáveis pelas ocorrências (RPM, BPM e CIA) e garantindo que os dados sejam apresentados de forma organizada
-  * e hierárquica.
-  ---------------------------------------------------------------------------------------------------------------------*/
--- SELEÇÃO DE COLUNAS ESPECÍFICAS PARA IDENTIFICAÇÃO DO DIGITADOR E A UNIDADE RESPONSÁVEL
+/*---------------------------------------------------------------------------------------------------------------------
+ * este código sql tem como objetivo calcular e apresentar informações detalhadas sobre o total de envolvidos nas 
+ * ocorrências registradas pela polícia militar de minas gerais no ano de 2024 e, mais especificamente, quantificar 
+ * quantos desses registros estão sem cpf ou cnpj informado. o foco é analisar ocorrências fechadas e filtrar por diversos 
+ * critérios para garantir a relevância e precisão dos dados, especialmente visando a clareza na identificação das unidades 
+ * responsáveis pelas ocorrências (rpm, bpm e cia) e garantindo que os dados sejam apresentados de forma organizada
+ * e hierárquica.
+ ---------------------------------------------------------------------------------------------------------------------*/
 SELECT
-    OCO.digitador_matricula AS MATRICULA_DIGITADOR, -- EXTRAI A MATRÍCULA DO DIGITADOR RESPONSÁVEL PELO REGISTRO
-    SPLIT_PART(OCO.unidade_responsavel_registro_nome, '/', -1) AS RPM, -- EXTRAI O ÚLTIMO SEGMENTO PARA IDENTIFICAR A RPM
-    SPLIT_PART(OCO.unidade_responsavel_registro_nome, '/', -2) AS BPM, -- EXTRAI O PENÚLTIMO SEGMENTO PARA IDENTIFICAR O BPM
-    SPLIT_PART(OCO.unidade_responsavel_registro_nome, '/', -3) AS CIA, -- EXTRAI O ANTEPENÚLTIMO SEGMENTO PARA IDENTIFICAR A CIA
-    COUNT(ENV.numero_envolvido) AS TOTAL_ENVOLVIDOS, -- CONTA O TOTAL DE ENVOLVIDOS NAS OCORRÊNCIAS
+    OCO.digitador_matricula AS MATRICULA_DIGITADOR, -- Extrai a matrícula do digitador responsável pelo registro
+    SPLIT_PART(OCO.unidade_responsavel_registro_nome, '/', -1) AS RPM, -- Extrai o último segmento para identificar a RPM
+    SPLIT_PART(OCO.unidade_responsavel_registro_nome, '/', -2) AS BPM, -- Extrai o penúltimo segmento para identificar o BPM
+    SPLIT_PART(OCO.unidade_responsavel_registro_nome, '/', -3) AS CIA, -- Extrai o antepenúltimo segmento para identificar a CIA
+    COUNT(ENV.numero_envolvido) AS TOTAL_ENVOLVIDOS, -- conta o total de envolvidos nas ocorrências
     SUM(CASE 
-        WHEN ENV.numero_cpf_cnpj IS NULL  -- CALCULA QUANTOS REGISTROS NÃO POSSUEM CPF/CNPJ INFORMADO
+        WHEN ENV.numero_cpf_cnpj IS NULL  -- Calcula quantos registros não possuem cpf/cnpj informado
         THEN 1 ELSE 0 
-        END) AS Qtd_Null
--- TABELAS E JUNÇÕES UTILIZADAS PARA A CONSULTA
+        END) AS Qtd_Null -- Soma os registros sem cpf/cnpj informado
+-- tabelas e junções utilizadas para a consulta
 FROM db_bisp_reds_reporting.tb_ocorrencia OCO
-JOIN db_bisp_reds_reporting.tb_envolvido_ocorrencia ENV ON ENV.numero_ocorrencia = OCO.numero_ocorrencia -- JUNÇÃO DAS TABELAS DE OCORRÊNCIA E ENVOLVIDOS
--- FILTROS APLICADOS PARA A SELEÇÃO DOS DADOS
-WHERE YEAR(ENV.data_hora_fato) = 2024  -- FILTRA OS DADOS PARA OCORRÊNCIAS DO ANO 2024
-  AND ENV.envolvimento_codigo IN ('1300', '1301', '1302', '1303', '1304', '1305', '1399') -- FILTRA PELOS CÓDIGOS DE ENVOLVIMENTO. APENAS VITIMAS.
-  AND OCO.relator_sigla_orgao = 'PM' -- APENAS OCORRÊNCIAS RELATADAS PELA POLÍCIA MILITAR
-  AND OCO.descricao_estado = 'FECHADO' -- FILTRA OCORRÊNCIAS QUE ESTÃO FECHADAS
-  AND ENV.condicao_fisica_codigo NOT IN ('0300', '0100') -- EXCLUI CERTAS CONDIÇÕES FÍSICAS 
-  AND OCO.ocorrencia_uf = 'MG' -- FILTRA OCORRÊNCIAS NO ESTADO DE MINAS GERAIS
-  AND OCO.nome_tipo_relatorio NOT IN ('RAT', 'BOS', 'BOS AMPLO') -- EXCLUI CERTOS TIPOS DE RELATÓRIOS
- -- AND OCO.unidade_responsavel_registro_nome LIKE '%/X BPM%' -- FILTRA OCORRÊNCIAS RELACIONADAS A UNIDADE DE REGISTRO.
--- AGRUPAMENTO DOS DADOS PARA ESTRUTURAR OS RESULTADOS
+JOIN db_bisp_reds_reporting.tb_envolvido_ocorrencia ENV ON ENV.numero_ocorrencia = OCO.numero_ocorrencia -- junção das tabelas de ocorrência e envolvidos
+-- filtros aplicados para a seleção dos dados
+WHERE 1 = 1
+  AND OCO.digitador_sigla_orgao = 'PM' -- Apenas ocorrências digitadas pela polícia militar
+  AND OCO.ocorrencia_uf = 'MG' -- Filtra ocorrências no estado de Minas Gerais
+  AND OCO.descricao_estado = 'FECHADO' -- Filtra ocorrências que estão fechadas
+  AND ENV.envolvimento_codigo IN ('1300', '1301', '1302', '1303', '1304', '1305', '1399') -- Filtra pelos códigos de envolvimento: apenas vítimas
+  AND ENV.condicao_fisica_codigo NOT IN ('0300', '0100') -- Exclui certas condições físicas
+  AND OCO.nome_tipo_relatorio NOT IN ('RAT', 'BOS', 'BOS AMPLO') -- Exclui certos tipos de relatórios
+  AND OCO.data_hora_fato BETWEEN '2025-01-01 00:00:00' AND '2025-05-01 00:00:00'  -- Filtra os dados para ocorrências dentro do intervalo especificado
+  -- AND OCO.unidade_responsavel_registro_nome LIKE '%/X BPM%' -- filtra ocorrências relacionadas à unidade de registro
+-- agrupamento dos dados para estruturar os resultados
 GROUP BY MATRICULA_DIGITADOR, RPM, BPM, CIA
--- ORDENAÇÃO DOS RESULTADOS PELA HIERARQUIA DA UNIDADE
-ORDER BY RPM, BPM, CIA
+-- ordenação dos resultados pela hierarquia da unidade
+ORDER BY MATRICULA_DIGITADOR,RPM, BPM, CIA;
