56e8c6e3d81114ff1f8a06f8e6a0161362c96694
CGA-PMMG <168194556+CGA-PMMG@users.noreply.github.com>
2025-06-10 14:18:10 -0300

    Update CAMPO_LAT_LONG_INVALIDO.sql

--- CAMPOS INVÁLIDOS/CAMPO_LAT_LONG_INVALIDO.sql
+++ CAMPOS INVÁLIDOS/CAMPO_LAT_LONG_INVALIDO.sql
@@ -1,11 +1,11 @@
 /*﻿-----------------------------------------------------------------------------------------------------------------------------------------
- * Este código SQL foi projetado para fornecer uma visão geral das ocorrências fechadas registradas pela Polícia Militar de Minas Gerais
- * no ano de 2024, focando em analisar o uso de instrumentos utilizados. O principal objetivo é contar o 
- * total de ocorrências únicas e determinar quantas delas não possuem informações sobre logitude e latitude.
+ * Este script SQL foi projetado para fornecer uma visão geral das ocorrências fechadas registradas pela Polícia Militar de Minas Gerais
+ *  no período especificado. O principal objetivo é contar o total de ocorrências únicas e determinar quantas delas não possuem 
+ * informações sobre logitude e latitude.
  ﻿-----------------------------------------------------------------------------------------------------------------------------------------*/
 SELECT
     OCO.digitador_matricula AS MATRICULA_DIGITADOR, -- Matrícula do digitador responsável pela entrada dos dados.
-    SPLIT_PART(OCO.unidade_responsavel_registro_nome, '/', -1) AS RPM, -- Extrai o último segmento da unidade responsável pelo registro 
+    SPLIT_PART(OCO.unidade_responsavel_registro_nome, '/', -1) AS RPM, -- Extrai o último segmento da unidade responsável pelo registro  
     SPLIT_PART(OCO.unidade_responsavel_registro_nome, '/', -2) AS BPM, -- Extrai o penúltimo segmento da unidade responsável pelo registro 
     SPLIT_PART(OCO.unidade_responsavel_registro_nome, '/', -3) AS CIA, -- Extrai o antepenúltimo segmento da unidade responsável pelo registro 
     COUNT(DISTINCT OCO.numero_ocorrencia) AS Total_Registros, -- Conta o total de registros únicos de ocorrências.
@@ -15,12 +15,13 @@ SELECT
 -- A fonte de dados vem da tabela de ocorrências.
 FROM db_bisp_reds_reporting.tb_ocorrencia OCO
 -- Aplica filtros para restringir os dados:
-WHERE YEAR(OCO.data_hora_fato) = 2024 -- Considera apenas ocorrências do ano de 2024.
-    AND OCO.nome_tipo_relatorio NOT IN('RAT', 'BOS', 'BOS AMPLO') -- Exclui tipos de relatórios que não são de interesse, como Relatório de Atendimento Tático (RAT) e Boletim de Ocorrência Simplificado (BOS).
-    AND OCO.relator_sigla_orgao = 'PM' -- Foca apenas nos registros inseridos pela Polícia Militar.
-    AND OCO.descricao_estado = 'FECHADO' -- Restringe a análise para casos já finalizados.
-    AND OCO.ocorrencia_uf = 'MG' -- Limita a consulta a ocorrências no estado de Minas Gerais.
-    -- AND OCO.unidade_responsavel_registro_nome LIKE '%/X BPM%' -- FILTRA OCORRÊNCIAS RELACIONADAS A UNIDADE DE REGISTRO.
+WHERE 1 = 1 
+  AND OCO.digitador_sigla_orgao = 'PM' -- Apenas ocorrências digitadas pela Polícia Militar
+  AND OCO.ocorrencia_uf = 'MG' -- Filtra ocorrências no estado de Minas Gerais
+  AND OCO.descricao_estado = 'FECHADO' -- Filtra ocorrências que estão fechadas
+  AND OCO.nome_tipo_relatorio NOT IN ('RAT', 'BOS', 'BOS AMPLO') -- Exclui certos tipos de relatórios
+  AND OCO.data_hora_fato BETWEEN '2025-01-01 00:00:00' AND '2025-05-01 00:00:00'  -- Filtra os dados para ocorrências dentro do intervalo especificado
+ -- AND OCO.unidade_responsavel_registro_nome LIKE '%/X BPM%' -- filtra ocorrências relacionadas à unidade de registro
 -- Agrupa os resultados pelas colunas selecionadas para permitir uma análise detalhada por unidade e digitador.
 GROUP BY
     OCO.digitador_matricula, RPM, BPM, CIA
