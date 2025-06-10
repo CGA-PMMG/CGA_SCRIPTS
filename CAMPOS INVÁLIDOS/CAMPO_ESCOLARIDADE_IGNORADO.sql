08a90cddf56cceae44342df61e0c46bdf19d190f
CGA-PMMG <168194556+CGA-PMMG@users.noreply.github.com>
2025-06-10 14:06:22 -0300

    Update CAMPO_ESCOLARIDADE_IGNORADO.sql

--- CAMPOS INVÁLIDOS/CAMPO_ESCOLARIDADE_IGNORADO.sql
+++ CAMPOS INVÁLIDOS/CAMPO_ESCOLARIDADE_IGNORADO.sql
@@ -1,21 +1,20 @@
 /*----------------------------------------------------------------------------------------------------------------------------------
  * Este código sql foi desenvolvido para fornecer uma análise detalhada sobre a integridade dos dados de escolaridade dos envolvidos 
- * em ocorrências fechadas registradas pela polícia militar de minas gerais no ano de 2024. o foco principal está em identificar 
+ * em ocorrências fechadas registradas pela Polícia Militar de Minas Gerais no período especificado. O objetivo está em identificar 
  * e quantificar os registros que não possuem informação sobre escolaridade ou que estão classificados com o código '9800' 
- * (informação ignorada). a consulta estrutura os resultados com base na hierarquia das unidades responsáveis (cia, bpm e rpm), 
- * permitindo uma visualização clara e ordenada para facilitar a interpretação e a análise dos dados.
+ * (ESCOLARIDADE - IGNORADA).
  ----------------------------------------------------------------------------------------------------------------------------------*/
--- seleciona colunas específicas para identificação do digitador e da unidade responsável
+-- seleciona colunas específicas para identificação do digitador e da unidade responsávelpelo registro
 SELECT
-    OCO.digitador_matricula AS MATRICULA_DIGITADOR, -- Extrai a matrícula do digitador responsável pelo registro
+    OCO.digitador_matricula AS MATRICULA_DIGITADOR, -- Seleciona a matrícula do digitador responsável pelo registro
     SPLIT_PART(OCO.unidade_responsavel_registro_nome, '/', -1) AS RPM, -- Extrai o último segmento da unidade responsável pelo registro
     SPLIT_PART(OCO.unidade_responsavel_registro_nome, '/', -2) AS BPM, -- Extrai o penúltimo segmento da unidade responsável pelo registro
     SPLIT_PART(OCO.unidade_responsavel_registro_nome, '/', -3) AS CIA, -- Extrai o antepenúltimo segmento da unidade responsável pelo registro
     COUNT(ENV.numero_envolvido) AS total_envolvidos, -- Conta o total de envolvidos nas ocorrências
     SUM(CASE 
-        WHEN ENV.escolaridade_codigo IS NULL OR ENV.escolaridade_codigo = '9800' -- Verifica ausência ou código específico de escolaridade
+        WHEN ENV.escolaridade_codigo IS NULL OR ENV.escolaridade_codigo = '9800' -- Verifica ausência ou ESCOLARIDADE - IGNORADA
         THEN 1 ELSE 0 
-        END) AS Qtd_Null_9800 -- Soma os registros com escolaridade ausente ou código '9800'
+        END) AS Qtd_Null_9800 -- Soma os registros com escolaridade ausente ou código '9800' (ESCOLARIDADE - IGNORADA)
 FROM db_bisp_reds_reporting.tb_ocorrencia OCO
 JOIN db_bisp_reds_reporting.tb_envolvido_ocorrencia ENV ON ENV.numero_ocorrencia = OCO.numero_ocorrencia -- junção das tabelas de ocorrência e envolvidos
 WHERE 1 = 1
