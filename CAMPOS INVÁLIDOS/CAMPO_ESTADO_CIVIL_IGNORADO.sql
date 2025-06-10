376490b6ca71b8a9fcc80c88ee56b4986f7ebbde
CGA-PMMG <168194556+CGA-PMMG@users.noreply.github.com>
2025-06-10 14:26:12 -0300

    Update CAMPO_ESTADO_CIVIL_IGNORADO.sql

--- CAMPOS INVÁLIDOS/CAMPO_ESTADO_CIVIL_IGNORADO.sql
+++ CAMPOS INVÁLIDOS/CAMPO_ESTADO_CIVIL_IGNORADO.sql
@@ -6,9 +6,9 @@
  ---------------------------------------------------------------------------------------------------------------------------------------*/
 SELECT
     OCO.digitador_matricula AS MATRICULA_DIGITADOR, -- Seleciona a matrícula do digitador responsável pelo registro
-    SPLIT_PART(OCO.unidade_responsavel_registro_nome, '/', -1) AS RPM, -- Extrai o último segmento para identificar a RPM
-    SPLIT_PART(OCO.unidade_responsavel_registro_nome, '/', -2) AS BPM, -- Extrai o penúltimo segmento para identificar o BPM
-    SPLIT_PART(OCO.unidade_responsavel_registro_nome, '/', -3) AS CIA, -- Extrai o antepenúltimo segmento para identificar a CIA
+    SPLIT_PART(OCO.unidade_responsavel_registro_nome, '/', -1) AS RPM, -- Extrai o último segmento da unidade responsável pelo registro para identificar a RPM
+    SPLIT_PART(OCO.unidade_responsavel_registro_nome, '/', -2) AS BPM, -- Extrai o penúltimo segmento da unidade responsável pelo registro para identificar o BPM
+    SPLIT_PART(OCO.unidade_responsavel_registro_nome, '/', -3) AS CIA, -- Extrai o antepenúltimo da unidade responsável pelo registro segmento para identificar a CIA
     COUNT(ENV.numero_envolvido) AS TOTAL_ENVOLVIDOS, -- Conta o número total de envolvidos na ocorrência
     SUM(CASE 
         WHEN ENV.estado_civil_codigo IS NULL OR ENV.estado_civil_codigo ='9800' -- Verifica estado civil ignorado (ESTADO CIVIL - IGNORADO)
