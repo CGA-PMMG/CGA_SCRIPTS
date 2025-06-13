514bb3723582dc7866019f73e410857cc9a3d984
CGA-PMMG <168194556+CGA-PMMG@users.noreply.github.com>
2025-06-13 09:24:25 -0300

    Update MAIOR_MENOR_APREENDIDO.sql

--- PRISÃO - APREENSÃO/MAIOR_MENOR_APREENDIDO.sql
+++ PRISÃO - APREENSÃO/MAIOR_MENOR_APREENDIDO.sql
@@ -1,8 +1,8 @@
 /*----------------------------------------------------------------------------------------------------------------------
- * O código SQL a seguir está projetado para analisar dados de ocorrências e envolvimentos registrados, especificamente 
- * para o ano de 2024, com o foco em prisões efetuadas pela Polícia Militar de Minas Gerais. O código está estruturado 
+ * O código SQL a seguir está projetado para analisar dados de ocorrências e envolvimentos registrados, com o foco em prisões
+ * efetuadas pela Polícia Militar de Minas Gerais. O código está estruturado 
  * para agrupar e contar o total de envolvidos por unidade operacional, baseando-se em diferentes 
- * faixas etárias (menores e maiores de idade).
+ * faixas etárias (menor e maior idade).
  ----------------------------------------------------------------------------------------------------------------------*/
 -- SELECIONA A UNIDADE OPERACIONAL BASEADA NO CÓDIGO DO MUNICÍPIO
 SELECT  
@@ -25,14 +25,14 @@ CASE WHEN OCO.codigo_municipio IN (310620) THEN '01 RPM'
 	    WHEN OCO.codigo_municipio IN (310120 , 310130 , 310140 , 310490 , 310720 , 310790 , 310830 , 310890 , 310910 , 310970 , 311050 , 311060 , 311360 , 311410 , 311480 , 311550 , 311720 , 311780 , 311790 , 311850 , 311900 , 311990 , 312050 , 312080 , 312110 , 312280 , 312440 , 312450 , 312510 , 312740 , 312920 , 313060 , 313240 , 313300 , 313310 , 313360 , 313490 , 313850 , 313990 , 314040 , 314190 , 314340 , 314380 , 314440 , 314600 , 314730 , 314760 , 314780 , 314910 , 315090 , 315100 , 315250 , 315260 , 315960 , 316200 , 316230 , 316320 , 316370 , 316440 , 316490 , 316540 , 316557 , 316580 , 316640 , 316700 , 316740 , 316780 , 316905 , 316910 , 316980 , 317170 , 317220) THEN'17 RPM'	
 	    WHEN OCO.codigo_municipio IN (310160 , 310190 , 310200 , 310260 , 310410 , 310430 , 310530 , 310760 , 310840 , 310950 , 311030 , 311100 , 311130 , 311160 , 311240 , 311280 , 311440 , 311470 , 311510 , 311640 , 311710 , 312120 , 312240 , 312340 , 312520 , 312630 , 312830 , 312870 , 312970 , 312990 , 313150 , 313290 , 313375 , 313480 , 313690 , 313900 , 314300 , 314320 , 314410 , 314510 , 314720 , 314790 , 315150 , 315170 , 315180 , 315290 , 315920 , 316220 , 316294 , 316390 , 316430 , 316470 , 316510 , 316690 , 317060) THEN '18 RPM'	
 	    WHEN OCO.codigo_municipio IN (316720 , 314930 , 314110 , 314740 , 315360 , 310990 , 310500 , 311250 , 313570 , 313100 , 310320 , 312720 , 311890 , 312640 , 310960 , 315850) THEN '19 RPM'	
-   	END AS RPM_2024,
+   	END AS RPM_2025,
 CASE WHEN OCO.codigo_municipio IN (310690,311590,311960,312130,312738,312850,314020,314950,315010,315540,315620,316290) THEN '02 BPM'
 		WHEN OCO.codigo_municipio IN (310240,311750,311810,312010,312100,312160,312260,312540,312550,312760,314250,314370,315330,316020,316050,316480,316590,316650,316710) THEN '03 BPM'
 		WHEN OCO.codigo_municipio IN (312125) THEN '04 BPM'
 		WHEN OCO.codigo_municipio IN (312770,310180,311265,312370,312580,313320,314995,316770,316840,310220,312690,314010,317150,316160,316300) THEN '06 BPM'
 		WHEN OCO.codigo_municipio IN (310020,310700,310740,311560,311980,312320,312470,313530,313720,313880,314050,314240,314350,314640,314890,315200,315370,316040,316660) THEN '07 BPM'
 		WHEN OCO.codigo_municipio IN (310080,310800,311120,311190,311200,311400,311450,311460,312020,313000,313040,313080,313430,313450,313820,313870,314460,314560,314770,314990,315060,315470,315880,315990,316120) THEN '08 BPM'
-		WHEN OCO.codigo_municipio IN (310163,310210,310290,310330,310560,310680,311630,312150,312500,312940,314570,314660,315030,315440,315730,315870,315940,316070,316620) THEN '09 BPM'
+		WHEN OCO.codigo_municipio IN (310163,310210,310290,310330,310560,310680,311630,312150,312500,312940,314570,314660,315030,315440,315730,315870,315940,316070) THEN '09 BPM'
 		WHEN OCO.codigo_municipio IN (314480,315390,315480) THEN '1 CIA PM IND'
 		WHEN OCO.codigo_municipio IN (310730,311650,311880,312380,312660,312735,312825,312960,313200,313680,313730,314545,316225,316265) THEN '10 BPM'
 		WHEN OCO.codigo_municipio IN (310370,311020,311170,311670,312400,314830,314880,316380,316850,317130) THEN '10 CIA PM IND'
@@ -100,7 +100,7 @@ CASE WHEN OCO.codigo_municipio IN (310690,311590,311960,312130,312738,312850,314
 		WHEN OCO.codigo_municipio IN (310810,312060,312600,313010,313220,313665,314070,315040,315530,316292) THEN '7 CIA PM IND'
 		WHEN OCO.codigo_municipio IN (310440,310460,311530,312290,313260,313840,314220,315410,315840,316443,315110,317210,310150,316000,312460) THEN '68 BPM'
 		WHEN OCO.codigo_municipio IN (313370,313380) THEN '9 CIA PM IND'
-		/* MUNIC PIOS COM MAIS DE 01 UEOP - 7 Munic pios*/
+		/* MUNIC PIOS COM MAIS DE 01 UEOP - 8 Munic pios*/
 		WHEN OCO.codigo_municipio =317020 AND (OCO.unidade_area_militar_nome LIKE '32 BPM%' or OCO.unidade_area_militar_nome LIKE '%/32 BPM%') AND (OCO.unidade_area_militar_nome not LIKE '%TM%')THEN '32 BPM'
 		WHEN OCO.codigo_municipio =317020 AND (OCO.unidade_area_militar_nome LIKE '17 BPM%' or OCO.unidade_area_militar_nome LIKE '%/17 BPM%') AND (OCO.unidade_area_militar_nome not LIKE '%TM%')THEN '17 BPM'
 		WHEN OCO.codigo_municipio =317010 AND (OCO.unidade_area_militar_nome LIKE '4 BPM%' or OCO.unidade_area_militar_nome LIKE '%/4 BPM%') AND (OCO.unidade_area_militar_nome not LIKE '%TM%')THEN '04 BPM'
@@ -121,26 +121,32 @@ CASE WHEN OCO.codigo_municipio IN (310690,311590,311960,312130,312738,312850,314
 		WHEN OCO.codigo_municipio =310620 AND (OCO.unidade_area_militar_nome LIKE '41 BPM%' or OCO.unidade_area_militar_nome LIKE '%/41 BPM%') AND (OCO.unidade_area_militar_nome not LIKE '%TM%')THEN '41 BPM'
 		WHEN OCO.codigo_municipio =310620 AND (OCO.unidade_area_militar_nome LIKE '49 BPM%' or OCO.unidade_area_militar_nome LIKE '%/49 BPM%') AND (OCO.unidade_area_militar_nome not LIKE '%TM%')THEN '49 BPM'
 		WHEN OCO.codigo_municipio =310620 AND (OCO.unidade_area_militar_nome LIKE '34 BPM%' or OCO.unidade_area_militar_nome LIKE '%/34 BPM%') AND (OCO.unidade_area_militar_nome not LIKE '%TM%')THEN '34 BPM'
+		WHEN OCO.codigo_municipio =316620 AND (OCO.unidade_area_militar_nome like '31 BPM%' or OCO.unidade_area_militar_nome like '%/31 BPM%') THEN '31 BPM'
+		WHEN OCO.codigo_municipio =316620 AND (OCO.unidade_area_militar_nome like '9 BPM%' or OCO.unidade_area_militar_nome like '%/9 BPM%') THEN '9 BPM'
 		ELSE 'OUTROS' 
-	END AS UEOP_2024,	
--- CONTAGEM DO NÚMERO DE ENVOLVIDOS EM CADA UNIDADE
-COUNT(ENV.numero_envolvido),
--- DETERMINAÇÃO DA FAIXA ETÁRIA BASEADA NA DIFERENÇA ENTRE O ANO DO FATO E O ANO DE NASCIMENTO
+	END AS UEOP_2025,		
+	 ibge.tipo_descricao,                              -- Informações adicionais do IBGE 
+	  OCO.unidade_area_militar_nome,                    -- Nome da unidade da área militar 
+	  MUB.udi,                                          
+	  MUB.ueop,                                         
+	  MUB.cia,                                          
+	  MUB.codigo_espacial_pm AS setor_PM,          
+COUNT(ENV.numero_envolvido) qtde_envolvido,
     CASE 
-        WHEN (YEAR(ENV.data_hora_fato) - YEAR(ENV.data_nascimento)) < 18 THEN 'MENOR IDADE'
+        WHEN ENV.valor_idade_aparente < 18 THEN 'MENOR IDADE'
         ELSE 'MAIOR IDADE'
     END AS FAIXA_ETARIA
--- ESPECIFICAÇÃO DAS TABELAS DE ONDE OS DADOS SÃO RETIRADOS E A CONDIÇÃO DE JUNÇÃO
-FROM db_bisp_reds_reporting.tb_ocorrencia OCO
-INNER JOIN db_bisp_reds_reporting.tb_envolvido_ocorrencia ENV ON OCO.numero_ocorrencia = ENV.numero_ocorrencia 
--- FILTROS PARA A SELEÇÃO DAS OCORRÊNCIAS: ANO, ORGÃO DIGITADOR, ESTADO, STATUS DA OCORRÊNCIA E CÓDIGOS DE PRISÃO
-WHERE YEAR(OCO.data_hora_fato) = 2024
-AND OCO.DIGITADOR_SIGLA_ORGAO = 'PM'
-AND OCO.ocorrencia_uf = 'MG'
-AND OCO.descricao_estado = 'FECHADO'
-AND ENV.tipo_prisao_apreensao_codigo IN ('9900', '0400', '0100', '0300', '0600', '0200')
---AND OCO.unidade_area_militar_nome LIKE '%X CIA PM IND/X RPM%' -- FILTRE SUA CIA/BPM/RPM
--- AGRUPAMENTO DOS RESULTADOS POR UNIDADE OPERACIONAL E FAIXA ETÁRIA PARA CONTAGEM CORRETA
-GROUP BY 1, 2, 4
--- ORDENAÇÃO DOS RESULTADOS POR UNIDADE E FAIXA ETÁRIA PARA APRESENTAÇÃO CLARA
-ORDER BY 1, 2, 4
+FROM db_bisp_reds_reporting.tb_ocorrencia OCOa
+ INNER JOIN db_bisp_reds_reporting.tb_envolvido_ocorrencia ENV ON OCO.numero_ocorrencia = ENV.numero_ocorrencia 
+ LEFT JOIN db_bisp_reds_master.tb_ocorrencia_setores_geodata AS geo ON OCO.numero_ocorrencia = geo.numero_ocorrencia AND OCO.ocorrencia_uf = 'MG'	-- Tabela de apoio que compara as lat/long com os setores IBGE		
+ LEFT JOIN db_bisp_shared.tb_ibge_setores_geodata AS ibge ON geo.setor_codigo = ibge.setor_codigo  -- Join esquerdo com tabela de dados IBGE enriquecidos 
+ LEFT JOIN db_bisp_shared.tb_pmmg_setores_geodata AS MUB  ON geo.setor_codigo = MUB.setor_codigo -- Join esquerdo com tabela MUB 
+WHERE 1 = 1 
+    AND OCO.digitador_id_orgao = 0 -- Filtra registros de ID do órgão 0 - PM
+    AND ENV.id_tipo_prisao_apreensao IN (1,2,3,6,4,7) -- Filtra determinados tipo de apreensão (FLAGRANTE DE ATO INFRACIONAL, FLAGRANTE DE CRIME / CONTRAVEN, MANDADO JUDICIAL, OUTRAS - PRISAO / APREENSAO, RECAPTURA, TCO/FLAGRANTE INFRACAO DE MENOR POTENCIAL OFENSIVO)
+	AND ENV.valor_idade_aparente BETWEEN 18 AND 30 -- Filtra faixa etária dentro do valor especificado
+    AND OCO.ind_estado = 'F'    -- Filtra apenas ocorrências fechadas
+    AND OCO.ocorrencia_uf = 'MG'      -- Filtra apenas ocorrências do estado de Minas Gerais
+	AND OCO.data_hora_fato BETWEEN '2025-05-01 00:00:00' AND '2025-05-10 00:00:00' -- Filtra data/hora fato dentro do intervalo especificado.
+GROUP BY 1, 2, 4, 5, 6, 7, 8, 3,10
+ORDER BY 1, 2, 4, 5, 6, 7, 8, 3,10
