b9c6c41bbb793f55c8d53462988476477c5f8b68
CGA-PMMG <168194556+CGA-PMMG@users.noreply.github.com>
2025-06-17 10:50:33 -0300

    Update VIATURAS_PRODUTIVIDADE.sql

--- PRODUTIVIDADE/VIATURAS_PRODUTIVIDADE.sql
+++ PRODUTIVIDADE/VIATURAS_PRODUTIVIDADE.sql
@@ -19,9 +19,9 @@ FROM
     db_bisp_reds_reporting.tb_viatura_ocorrencia as VT -- Tabela que registra as viaturas envolvidas em ocorrências
  LEFT JOIN 
     db_bisp_reds_reporting.tb_ocorrencia as OCO ON OCO.numero_ocorrencia = VT.numero_ocorrencia  -- Junção com a tabela de ocorrências
-WHERE  1 = 1
-    AND OCO.data_hora_fato BETWEEN '2025-02-27 00:00:00.000' AND '2025-02-28 23:59:59.000' -- Filtra ocorrências por período específico
-	AND OCO.ocorrencia_uf = 'MG'      -- Filtra apenas ocorrências do estado de Minas Gerais
-    	AND OCO.digitador_sigla_orgao = 'PM'      -- Filtro por ocorrências Polícia Militar 
-	--  AND OCO.unidade_responsavel_registro_nome LIKE '%xx BPM/xx RPM%' -- Filtra pelo nome da unidade responsável pelo registro
-;	--  AND OCO.codigo_municipio IN (123456,456789,987654,......) -- PARA RESGATAR APENAS OS DADOS DOS MUNICÍPIOS SOB SUA RESPONSABILIDADE, REMOVA O COMENTÁRIO E ADICIONE O CÓDIGO DE MUNICIPIO DA SUA RESPONSABILIDADE. NO INÍCIO DO SCRIPT, É POSSÍVEL VERIFICAR ESSES CÓDIGOS, POR RPM E UEOP.
+WHERE 1 = 1 
+AND OCO.digitador_id_orgao = 0 -- Filtra pelo ID do órgão digitador - PM 
+AND OCO.ocorrencia_uf ='MG'                  -- Filtra apenas ocorrências de Minas Gerais
+AND OCO.data_hora_fato BETWEEN '2025-01-01 00:00:00.000' AND '2025-02-01 00:00:00.000'  -- Filtra ocorrências entre o periodo especificado
+ --  AND OCO.unidade_responsavel_registro_nome LIKE '%xx BPM/xx RPM%' -- Filtra pelo nome da unidade responsável pelo registro
+	--  AND OCO.codigo_municipio IN (123456,456789,987654,......) -- PARA RESGATAR APENAS OS DADOS DOS MUNICÍPIOS SOB SUA RESPONSABILIDADE, REMOVA O COMENTÁRIO E ADICIOUEOP.
