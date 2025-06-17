6209d7d7f1b509ef5ef1eb35902215cce6cbdae6
CGA-PMMG <168194556+CGA-PMMG@users.noreply.github.com>
2025-06-17 10:34:27 -0300

    Update MILITAR_RAT_PRODUTIVIDADE.sql

--- PRODUTIVIDADE/MILITAR_RAT_PRODUTIVIDADE.sql
+++ PRODUTIVIDADE/MILITAR_RAT_PRODUTIVIDADE.sql
@@ -2,7 +2,7 @@
  * Este script SQL tem como finalidade fornecer uma visão detalhada dos membros das equipes que atuaram em 
  * ocorrências relacionadas ao RAT Produtividade, destacando suas respectivas funções, unidades de lotação, 
  * a descrição do indicador do Rat e sua quantidade. A consulta integra informações das tabelas de 
- * integrante da guarnição, ocorrências e RAT produtividade, permitindo uma análise focada no desempenho de equipes. 
+ * integrante da guarnição e RAT produtividade, permitindo uma análise focada no desempenho de equipes. 
 --------------------------------------------------------------------------------------------------------------------*/
 SELECT 
     IG.numero_matricula, -- Matrícula do integrante
@@ -22,12 +22,11 @@ FROM
  INNER JOIN  
     db_bisp_reds_reporting.tb_rat_produtividade_ocorrencia as RAT-- Junção com a tabela Rat Produtividade para retornar dados da produtividade do rat
     ON RAT.numero_ocorrencia = IG.numero_ocorrencia 
-WHERE 
-    YEAR(OCO.data_hora_fato) = 2025 -- Filtra ano da data/hora fato
-    AND MONTH (OCO.data_hora_fato) = 6 -- Filtra o mês da data/hora fato
-    AND OCO.ocorrencia_uf = 'MG' -- Filtra ocorrências no estado de MG
-    AND OCO.digitador_id_orgao = 0 -- Garante que retorne ocorrências atendidas apenas por policiais militares
-	--  AND OCO.unidade_responsavel_registro_nome LIKE '%xx BPM/xx RPM%' -- Filtra pelo nome da unidade responsável pelo registro
-	--  AND OCO.codigo_municipio IN (123456,456789,987654,......) -- PARA RESGATAR APENAS OS DADOS DOS MUNICÍPIOS SOB SUA RESPONSABILIDADE, REMOVA O COMENTÁRIO E ADICIONE O CÓDIGO DE MUNICIPIO DA SUA RESPONSABILIDADE. NO INÍCIO DO SCRIPT, É POSSÍVEL VERIFICAR ESSES CÓDIGOS, POR RPM E UEOP.
-    --  AND IG.numero_matricula = 'xxxx' -- Filtre pela matrícula do militar 
-ORDER BY IG.numero_matricula, IG.data_hora_fato, IG.numero_ocorrencia -- Ordena por número da matricula do militar, data/hora do fato e pelo número da ocorrência
+WHERE 1 = 1 
+AND OCO.digitador_id_orgao = 0 -- Filtra pelo ID do órgão digitador - PM 
+AND OCO.ocorrencia_uf ='MG'                  -- Filtra apenas ocorrências de Minas Gerais
+AND OCO.nome_tipo_relatorio = 'RAT'
+AND OCO.data_hora_fato BETWEEN '2025-01-01 00:00:00.000' AND '2025-02-01 00:00:00.000'  -- Filtra ocorrências entre o periodo especificado
+ -- AND OCO.unidade_area_militar_nome LIKE '%x BPM/x RPM%' -- Filtra pelo nome da unidade área militar
+	-- AND OCO.unidade_responsavel_registro_nome LIKE '%xx RPM%' -- Filtra pelo nome da unidade responsável pelo registro
+	-- AND OCO.codigo_municipio IN (123456,456789,987654,......) -- PARA RESGATAR APENAS OS DADOS DOS MUNICÍPIOS SOB SUA RESPONSABILIDADE, REMOVA O COMENTÁRIO E ADICIONE O CÓDIGO DE MUNICIPIO DA SUA RESPONSABILIDADE. NO INÍCIO DO SCRIPT, É POSSÍVEL VERIFICAR ESSES CÓDIGOS, POR RPM E UEOP.
