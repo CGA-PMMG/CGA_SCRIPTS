6a635bed2765da000654785d0f1390dc4fa529de
CGA-PMMG <168194556+CGA-PMMG@users.noreply.github.com>
2025-06-17 10:46:56 -0300

    Update INTEGRANTES_PRODUTIVIDADE.sql

--- PRODUTIVIDADE/INTEGRANTES_PRODUTIVIDADE.sql
+++ PRODUTIVIDADE/INTEGRANTES_PRODUTIVIDADE.sql
@@ -11,14 +11,16 @@ SELECT
     IG.nome, -- Nome do integrante
     IG.unidade_servico_codigo, -- Código da unidade de serviço
     IG.unidade_servico_nome, -- Nome da unidade de serviço
+    IG.unidade_responsavel_registro_nome, -- Nome da unidade responsável pelo registro
     IG.data_hora_fato -- Data e hora do fato
 FROM 
     db_bisp_reds_reporting.tb_integrante_guarnicao_ocorrencia  AS IG-- Tabela de integrantes de guarnição
  LEFT JOIN 
     db_bisp_reds_reporting.tb_ocorrencia AS OCO ON OCO.numero_ocorrencia = IG.numero_ocorrencia -- Junção com a tabela de ocorrências
-WHERE  1 = 1
-    AND OCO.data_hora_fato BETWEEN '2025-01-01 00:00:00.000' AND '2025-02-28 23:59:59.000' -- Filtra ocorrências por período específico (todo o ano de 2024 até fevereiro/2025)
-	AND OCO.ocorrencia_uf = 'MG'      -- Filtra apenas ocorrências do estado de Minas Gerais
-    --	AND OCO.digitador_sigla_orgao = 'PM'      -- Filtro por ocorrências Polícia Militar 
-	--  AND OCO.unidade_responsavel_registro_nome LIKE '%xx BPM/xx RPM%' -- Filtra pelo nome da unidade responsável pelo registro
-	--  AND OCO.codigo_municipio IN (123456,456789,987654,......) -- PARA RESGATAR APENAS OS DADOS DOS MUNICÍPIOS SOB SUA RESPONSABILIDADE, REMOVA O COMENTÁRIO E ADICIONE O CÓDIGO DE MUNICIPIO DA SUA RESPONSABILIDADE. NO INÍCIO DO SCRIPT, É POSSÍVEL VERIFICAR ESSES CÓDIGOS, POR RPM E UEOP.
+WHERE 1 = 1 
+AND OCO.digitador_id_orgao = 0 -- Filtra pelo ID do órgão digitador - PM 
+AND OCO.ocorrencia_uf ='MG'                  -- Filtra apenas ocorrências de Minas Gerais
+AND OCO.data_hora_fato BETWEEN '2025-01-01 00:00:00.000' AND '2025-02-01 00:00:00.000'  -- Filtra ocorrências entre o periodo especificado
+ -- AND IG.unidade_servico_nome LIKE '%x BPM/x RPM%' -- Filtra pelo nome da unidade área militar
+	-- AND IG.unidade_responsavel_registro_nome LIKE '%xx RPM%' -- Filtra pelo nome da unidade responsável pelo registro
+	-- AND OCO.codigo_municipio IN (123456,456789,987654,......) -- PARA RESGATAR APENAS OS DADOS DOS MUNICÍPIOS SOB SUA RESPONSABILIDADE, REMOVA O COMENTÁRIO E ADICIONE O CÓDIGO DE MUNICIPIO DA SUA RESPONSABILIDADE. NO INÍCIO DO SCRIPT, É POSSÍVEL VERIFICAR ESSES CÓDIGOS, POR RPM E UEOP.CÓDIGOS, POR RPM E UEOP.
