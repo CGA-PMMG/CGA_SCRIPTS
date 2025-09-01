88584084490678a6dc45de3b6182c4a60988cb7f
CGA-PMMG <168194556+CGA-PMMG@users.noreply.github.com>
2025-09-01 13:25:49 -0300

    Update EMPENHO_CAD.sql

--- CAD/EMPENHO_CAD.sql
+++ CAD/EMPENHO_CAD.sql
@@ -3,43 +3,35 @@
  * eventos, empenhos e integrações de registros de eventos específicos dentro de um banco de dados CAD de relatórios 
  * da Polícia Militar. O foco é obter dados para chamadas atendidas por unidades específicas da PM.
  ---------------------------------------------------------------------------------------------------------------*/
--- SELECIONA AS COLUNAS DE INTERESSE DAS TABELAS CORRESPONDENTES
-SELECT 
-    AT.chamada_id,                              -- ID DA CHAMADA DE ATENDIMENTO
-    AT.chamada_numero,                          -- NÚMERO DA CHAMADA DE ATENDIMENTO
-    EV.id_evento,                                    -- ID DO EVENTO ASSOCIADO
-    EV.descricao_evento,                             -- DESCRIÇÃO DO EVENTO
-    AT.data_hora_inicio_n,                      -- DATA E HORA DE INÍCIO DO ATENDIMENTO
-    AT.data_hora_fim_n,                         -- DATA E HORA DE FIM DO ATENDIMENTO
-    CA.chamada_id,                      -- ID DA CHAMADA NA TABELA DE CHAMADAS
-    CA.chamada_numero,                  -- NÚMERO DA CHAMADA
-    CA.chamada_data_hora_inclusao,      -- DATA E HORA DE INCLUSÃO DA CHAMADA
-    CA.chamada_classificacao_data_hora, -- DATA E HORA DA CLASSIFICAÇÃO DA CHAMADA
-    CA.chamada_classificacao_descricao, -- DESCRIÇÃO DA CLASSIFICAÇÃO DA CHAMADA
-    CA.orgao_sigla,                     -- SIGLA DO ÓRGÃO (PM)
-    CA.natureza_codigo,                 -- CÓDIGO DA NATUREZA DA CHAMADA
-    CA.natureza_descricao,              -- DESCRIÇÃO DA NATUREZA DA CHAMADA
-    CA.unidade_servico_nome,            -- NOME DA UNIDADE DE SERVIÇO
-    CA.chamada_atendimento_id,          -- ID DO ATENDIMENTO DA CHAMADA
-    EMP.recurso_tipo_empenho,                        -- TIPO DE EMPENHO DO RECURSO
-    EMP.tipo_recurso_codigo,                         -- CÓDIGO DO TIPO DE RECURSO
-    EMP.tipo_recurso_descricao,                      -- DESCRIÇÃO DO TIPO DE RECURSO
-    EMP.viatura_numero_prefixo,                      -- PREFIXO DA VIATURA
-    EMP.empenho_data_hora_inicio,                    -- DATA E HORA DE INÍCIO DO EMPENHO
-    EMP.empenho_data_hora_fim,                       -- DATA E HORA DE FIM DO EMPENHO
-    REDS.reds_numero, -- NÚMERO DO REDS
-    SIT_EMP.empenho_situacao_descricao,                 -- DESCRIÇÃO DA SITUAÇÃO DO EMPENHO
-    SIT_EMP.empenho_situacao_data_hora_inicio,          -- DATA E HORA DE INÍCIO DA SITUAÇÃO DO EMPENHO
-    SIT_EMP.empenho_situacao_data_hora_fim              -- DATA E HORA DE FIM DA SITUAÇÃO DO EMPENHO
--- ESPECIFICA AS TABELAS E AS JUNÇÕES NECESSÁRIAS PARA ACESSAR AS INFORMAÇÕES
-FROM db_bisp_cad_reporting.tb_atendimento AS AT
-LEFT JOIN  db_bisp_cad_reporting.tb_chamada_atendimento AS CA ON AT.chamada_id = CA.chamada_id
-LEFT JOIN db_bisp_cad_reporting.tb_empenho AS EMP ON CA.chamada_atendimento_id = EMP.chamada_atendimento_id
-LEFT JOIN db_bisp_cad_reporting.tb_empenho_situacao AS SIT_EMP ON EMP.empenho_id = SIT_EMP.empenho_id    
-LEFT JOIN db_bisp_cad_reporting.vw_chamada_evento AS EV ON EMP.chamada_atendimento_id = EV.id_chamada_atendimento
-LEFT JOIN  db_bisp_cad_reporting.tb_integracao_reds AS REDS ON REDS.chamada_atendimento_id = EMP.chamada_atendimento_id
--- CONDIÇÕES PARA A FILTRAGEM DOS DADOS
+SELECT
+EMP.recurso_tipo_empenho,                        -- TIPO DE EMPENHO DO RECURSO
+EMP.tipo_recurso_codigo,                         -- CÓDIGO DO TIPO DE RECURSO
+EMP.tipo_recurso_descricao,                      -- DESCRIÇÃO DO TIPO DE RECURSO
+EMP.viatura_numero_prefixo,                      -- PREFIXO DA VIATURA
+EMP.empenho_data_hora_inicio,                    -- DATA E HORA DE INÍCIO DO EMPENHO
+EMP.empenho_data_hora_fim,                       -- DATA E HORA DE FIM DO EMPENHO 
+CA.chamada_id,                      -- ID DA CHAMADA NA TABELA DE CHAMADAS
+CA.chamada_numero,                  -- NÚMERO DA CHAMADA
+CA.chamada_data_hora_inclusao,      -- DATA E HORA DE INCLUSÃO DA CHAMADA
+CA.chamada_classificacao_data_hora, -- DATA E HORA DA CLASSIFICAÇÃO DA CHAMADA
+CA.chamada_classificacao_descricao, -- DESCRIÇÃO DA CLASSIFICAÇÃO DA CHAMADA
+CA.orgao_sigla,                     -- SIGLA DO ÓRGÃO (PM)
+CA.natureza_codigo,                 -- CÓDIGO DA NATUREZA DA CHAMADA
+CA.natureza_descricao,              -- DESCRIÇÃO DA NATUREZA DA CHAMADA
+CA.unidade_servico_nome,            -- NOME DA UNIDADE DE SERVIÇO
+SPLIT_PART(LO.unidade_area_militar_nome,'/',-1) RPM, -- PRIMEIRA POSIÇÃO DA DIRETA PRA ESQUERDA DA UNIDADE DE AREA MILITAR, REPRESENTA A RPM
+SPLIT_PART(LO.unidade_area_militar_nome,'/',-2) UEOP, -- SEGUNDA POSIÇÃO DA DIRETA PRA ESQUERDA DA UNIDADE DE AREA MILITAR, REPRESENTA A UEOP
+LO.unidade_area_militar_nome -- NOME DA UNIDADE DE AREA MILITAR 
+FROM db_bisp_cad_reporting.tb_empenho EMP 
+INNER JOIN db_bisp_cad_reporting.tb_chamada_atendimento CA ON EMP.chamada_atendimento_id = CA.chamada_atendimento_id
+INNER JOIN db_bisp_cad_reporting.tb_chamada TC ON TC.id_chamada = CA.chamada_id
+LEFT JOIN db_bisp_reds_master.tb_local_unidade_area_pmmg LO ON LO.id_local = TC.id_local 
+--INNER JOIN db_bisp_reds_reporting.tb_ocorrencia AS OCO ON CA.chamada_numero = OCO.numero_chamada_cad  
 WHERE 1 = 1
-    AND CA.orgao_sigla = 'PM' -- FILTRA PELA SIGLA DA POLÍCIA MILITAR
-    AND CA.unidade_servico_nome LIKE '%/X BPM%'-- FILTRA PELAS UNIDADES ESPECÍFICAS DA PM
-    AND AT.data_hora_inicio_n BETWEEN '2025-01-01 00:00:00' AND '2025-02-01 00:00:00'
+AND EMP.unidade_servico_codigo_tipo  = 'PM' 
+AND EMP.tipo_recurso_codigo = 'x'
+AND EMP.viatura_numero_prefixo IN ('123a', '123b',' 123c')
+--AND CA.unidade_servico_nome LIKE '%22 BPM%'-- FILTRA PELAS UNIDADES ESPECÍFICAS DA PM
+--AND LO.unidade_area_militar_nome LIKE '%22 BPM%'-- FILTRA PELAS UNIDADES ESPECÍFICAS DA PM
+AND EMP.empenho_data_hora_inicio  BETWEEN '2025-01-01 00:00:00' AND '2025-08-24 23:59:59'
+ORDER BY EMP.viatura_numero_prefixo, EMP.empenho_data_hora_inicio
