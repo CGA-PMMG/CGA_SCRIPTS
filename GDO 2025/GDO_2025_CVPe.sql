161ecaa92caba16ff719934392c91d36ef358b1f
CGA-PMMG <168194556+CGA-PMMG@users.noreply.github.com>
2025-09-01 08:41:07 -0300

    Update GDO_2025_CVPe.sql

--- GDO 2025/GDO_2025_CVPe.sql
+++ GDO 2025/GDO_2025_CVPe.sql
@@ -2,7 +2,74 @@
  *Script do indicador CVPe(Crimes Violentos Contra a Pessoa) tem por finalidade avaliar a quantidade de vítimas de
  * Homicídio,  Tortura,  Sequestro e Cárcere Privado e Feminicídio*.
  -----------------------------------------------------------------------------------------------------------------------------*/
-WITH LETALIDADE AS                                                        -- Define uma tabela temporária para filtrar ocorrências com letalidade policial
+WITH 
+AGUAS AS (
+SELECT '310800810000009' AS setor_codigo, 'Rural'  AS zona_agua UNION ALL
+    SELECT '311440205000086', 'Rural'  UNION ALL
+    SELECT '312520005000014', 'Rural'  UNION ALL
+    SELECT '312570510000021', 'Rural'  UNION ALL
+    SELECT '312610910000009', 'Rural'  UNION ALL
+    SELECT '315180005000418', 'Rural'  UNION ALL
+    SELECT '315430905000031', 'Urbana' UNION ALL
+    SELECT '315690810000020', 'Rural'  UNION ALL
+    SELECT '316225205000015', 'Urbana' UNION ALL
+    SELECT '316720205010026', 'Urbana' UNION ALL
+    SELECT '316935605000093', 'Urbana' UNION ALL
+    SELECT '310160705000238', 'Rural'  UNION ALL
+    SELECT '310160705000241', 'Rural'  UNION ALL
+    SELECT '310430405000063', 'Rural'  UNION ALL
+    SELECT '310430405000071', 'Rural'  UNION ALL
+    SELECT '311130905000043', 'Rural'  UNION ALL
+    SELECT '311860105280029', 'Urbana' UNION ALL
+    SELECT '312570508000008', 'Rural'  UNION ALL
+    SELECT '312700805000039', 'Urbana' UNION ALL
+    SELECT '313760105000244', 'Urbana' UNION ALL
+    SELECT '314330205000733', 'Urbana' UNION ALL
+    SELECT '314350005000030', 'Rural'  UNION ALL
+    SELECT '315050510000006', 'Rural'  UNION ALL
+    SELECT '316720205010022', 'Urbana' UNION ALL
+    SELECT '310160710000006', 'Rural'  UNION ALL
+    SELECT '310430405000056', 'Rural'  UNION ALL
+    SELECT '310430405000058', 'Rural'  UNION ALL
+    SELECT '310430405000065', 'Rural'  UNION ALL
+    SELECT '310710905000056', 'Rural'  UNION ALL
+    SELECT '314990305000064', 'Rural'  UNION ALL
+    SELECT '316130405000021', 'Rural'  UNION ALL
+    SELECT '316935610000016', 'Rural'  UNION ALL
+    SELECT '310110205000035', 'Urbana' UNION ALL
+    SELECT '312520005000012', 'Rural'  UNION ALL
+    SELECT '312710705000131', 'Rural'  UNION ALL
+    SELECT '313115805000020', 'Urbana' UNION ALL
+    SELECT '314350010000007', 'Rural'  UNION ALL
+    SELECT '316250030000015', 'Rural'  UNION ALL
+    SELECT '316720205020024', 'Urbana' UNION ALL
+    SELECT '310070805000015', 'Rural'  UNION ALL
+    SELECT '312700805000037', 'Rural'  UNION ALL
+    SELECT '312700820000008', 'Urbana' UNION ALL
+    SELECT '313000205000017', 'Rural'  UNION ALL
+    SELECT '313450905000031', 'Urbana' UNION ALL
+    SELECT '314460720000006', 'Rural'  UNION ALL
+    SELECT '315180005000419', 'Rural'  UNION ALL
+    SELECT '310430405000068', 'Rural'  UNION ALL
+    SELECT '310670505090031', 'Urbana' UNION ALL
+    SELECT '311160605000065', 'Rural'  UNION ALL
+    SELECT '311160610000011', 'Rural'  UNION ALL
+    SELECT '311460005000033', 'Rural'  UNION ALL
+    SELECT '312070605000007', 'Urbana' UNION ALL
+    SELECT '312570505000047', 'Rural'  UNION ALL
+    SELECT '316250010000008', 'Rural'  UNION ALL
+    SELECT '311440210000014', 'Rural'  UNION ALL
+    SELECT '312570510000030', 'Rural'  UNION ALL
+    SELECT '312610925000020', 'Rural'  UNION ALL
+    SELECT '313040805000023', 'Rural'  UNION ALL
+    SELECT '313430120000010', 'Rural'  UNION ALL
+    SELECT '313450905000023', 'Rural'  UNION ALL
+    SELECT '313820305000223', 'Rural'  UNION ALL
+    SELECT '315050505000027', 'Rural'  UNION ALL
+    SELECT '315160205000036', 'Rural'  UNION ALL
+    SELECT '317010705000100', 'Rural'
+), -- !!!!!  ESTA CTE NÃO DEVE SER ALTERADA !!!!!
+LETALIDADE AS                                                        -- Define uma tabela temporária para filtrar ocorrências com letalidade policial
 ( 
     SELECT                                                                 -- Inicia a seleção dos campos para a CTE
         ENV.numero_ocorrencia,                                            -- Seleciona o número identificador da ocorrência
@@ -158,14 +225,14 @@ CASE WHEN OCO.codigo_municipio IN (310690,311590,311960,312130,312738,312850,314
     OCO.unidade_responsavel_registro_nome,                        -- Nome da unidade que registrou a ocorrência
     geo.latitude_sirgas2000,				-- reprojeção da latitude de SAD69 para SIRGAS2000
     geo.longitude_sirgas2000,				-- reprojeção da longitude de SAD69 para SIRGAS2000
-    CASE 																			-- se o território é Urbano ou Rural segundo o IBGE
-    	WHEN OCO.pais_codigo <> 1 AND OCO.ocorrencia_uf IS NULL THEN 'Outro_Pais'  	-- trata erro - ocorrencia de fora do Brasil
-	   WHEN OCO.ocorrencia_uf <> 'MG' THEN 'Outra_UF'								-- trata erro - ocorrencia de fora de MG
-    	WHEN OCO.numero_latitude IS NULL THEN 'Invalido'							-- trata erro - ocorrencia sem latitude
-        WHEN geo.situacao_codigo = 9 THEN 'Agua'									-- trata erro - ocorrencia dentro de curso d'água
+CASE 																			-- se o território é Urbano ou Rural segundo o IBGE
+    	WHEN oco.pais_codigo <> 1 AND oco.ocorrencia_uf IS NULL THEN 'Outro_Pais'  	-- trata erro - ocorrencia de fora do Brasil
+		WHEN oco.ocorrencia_uf <> 'MG' THEN 'Outra_UF'								-- trata erro - ocorrencia de fora de MG
+    	WHEN oco.numero_latitude IS NULL THEN 'Invalido'							-- trata erro - ocorrencia sem latitude
+        WHEN geo.situacao_codigo = 9 THEN AG.zona_agua									-- trata erro - ocorrencia dentro de curso d'água
        	WHEN geo.situacao_zona IS NULL THEN 'Erro_Processamento'					-- checa se restou alguma ocorrencia com erro
     	ELSE geo.situacao_zona
-    END AS situacao_zona,
+END AS situacao_zona,  
     ibge.tipo_descricao,                              -- Informações adicionais do IBGE 
     CAST(OCO.codigo_municipio AS INTEGER),                        -- Converte o código do município para número inteiro
     OCO.nome_municipio,                                           -- Nome do município da ocorrência
@@ -195,6 +262,7 @@ LEFT JOIN LETALIDADE LET ON OCO.numero_ocorrencia = LET.numero_ocorrencia -- Joi
 LEFT JOIN db_bisp_reds_master.tb_ocorrencia_setores_geodata AS geo ON OCO.numero_ocorrencia = geo.numero_ocorrencia	-- Tabela de apoio que compara as lat/long com os setores IBGE		
 LEFT JOIN db_bisp_shared.tb_ibge_setores_geodata AS ibge ON geo.setor_codigo = ibge.setor_codigo  -- Join esquerdo com tabela de dados IBGE enriquecidos 
 LEFT JOIN db_bisp_shared.tb_pmmg_setores_geodata AS MUB  ON geo.setor_codigo = MUB.setor_codigo -- Join esquerdo com tabela MUB 
+ LEFT JOIN AGUAS AG ON geo.setor_codigo = AG.setor_codigo
 WHERE 1=1                                                           -- Início das condições de filtro
     AND LET.numero_ocorrencia IS NULL                              -- Exclui ocorrências que estão na CTE de letalidade
     AND OCO.ind_estado = 'F'                                      -- Filtra apenas ocorrências finalizadas
