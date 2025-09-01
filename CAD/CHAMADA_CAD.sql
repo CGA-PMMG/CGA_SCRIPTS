98689e4a6e3cf1440e7a95d84b8dcdfe3550e939
CGA-PMMG <168194556+CGA-PMMG@users.noreply.github.com>
2025-09-01 16:13:42 -0300

    Update CHAMADA_CAD.sql

--- CAD/CHAMADA_CAD.sql
+++ CAD/CHAMADA_CAD.sql
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------------------------------------
- * Contagem de chamadas realizadas no CAD em 2023 e 2024 
+ * Contagem de chamadas realizadas no CAD em 2023 e 2025
  ---------------------------------------------------------------------------------------------------------*/
 -- Seleciona o ID e o nome do município das chamadas de atendimento
 SELECT 
@@ -11,14 +11,14 @@ SELECT
 FROM db_bisp_cad_reporting.tb_chamada_atendimento CA
 WHERE 1 = 1
     AND CA.orgao_sigla = 'PM'  -- Filtra as chamadas atendidas pela Polícia Militar
-    AND CA.chamada_data_hora_inclusao BETWEEN '2023-01-01 00:00:00.000' AND '2025-05-31 23:59:59.000'  -- Seleciona chamadas entre o início de 2023 e o final de março de 202
+    AND CA.chamada_data_hora_inclusao BETWEEN '2023-01-01 00:00:00.000' AND '2025-08-31 23:59:59.000'  -- Seleciona chamadas entre o início de 2023 e o final de março de 202
 GROUP BY CA.local_municipio_id, CA.local_municipio_nome -- Agrupa os resultados pelo ID e nome do município
 ORDER BY CA.local_municipio_nome, CA.local_municipio_id; -- Ordena os resultados primeiro pelo nome do município e depois pelo ID do município
 
 
 
 /*----------------------------------------------------------------------------------------------------------
- * Contagem de chamadas realizadas no CAD em 2023 e 2024 atendidas pelo recurso específico
+ * Contagem de chamadas realizadas no CAD em 2023 e 2025 atendidas pelo recurso específico
  ---------------------------------------------------------------------------------------------------------*/
 SELECT 
     CA.local_municipio_id, -- Seleciona o ID do município
@@ -34,7 +34,6 @@ ON CA.chamada_id = EMP.chamada_atendimento_id
 WHERE 1 = 1
     AND CA.orgao_sigla = 'PM'    -- Filtra as chamadas atendidas pela Polícia Militar
     AND EMP.tipo_recurso_codigo = 'RP'   -- Filtra por empenhos que usam o tipo de recurso com código 'XX',exemplo: RP (RADIOPATRULHA)
-    AND CA.chamada_data_hora_inclusao BETWEEN '2023-01-01 00:00:00.000' AND '2025-05-31 23:59:59.000'  -- Seleciona chamadas entre o início de 2023 e o final de março de 202
+    AND CA.chamada_data_hora_inclusao BETWEEN '2023-01-01 00:00:00.000' AND '2025-08-31 23:59:59.000'  -- Seleciona chamadas entre o início de 2023 e o final de março de 202
 GROUP BY CA.local_municipio_id, CA.local_municipio_nome,EMP.tipo_recurso_codigo,EMP.tipo_recurso_descricao  -- Agrupa os resultados pelo ID e nome do município para contagem
 ORDER BY CA.local_municipio_nome, CA.local_municipio_id,EMP.tipo_recurso_codigo,EMP.tipo_recurso_descricao ; -- Ordena os resultados pelo nome do município e pelo ID do município para facilitar a visualização
-
